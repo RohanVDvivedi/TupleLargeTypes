a86e8f13eedbf44ec27a397bf2f6ff9f65ca5451
RohanVDvivedi <rohandvivedi@gmail.com>
2025-02-05 22:22:47 +0530

    startedwith adding blob short and large types

---
+++ inc/blob_large.h
@@ -0,0 +1,10 @@
+#ifndef BLOB_LARGE_H
+#define BLOB_LARGE_H
+
+#include<data_type_info.h>
+#include<page_access_specification.h>
+
+// returns a new type info pointing to to a tuple, of 2 elements the blob_short_p, and a page_id type from pas_p
+data_type_info* get_blob_large_type_info(const data_type_info* blob_short_p, const page_access_specs* pas_p);
+
+#endif
