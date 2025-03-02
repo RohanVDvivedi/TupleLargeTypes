015623da30dba57afceed78dc865147681d0de22
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-02 21:56:42 +0530

    implemented constructor for the text blob read iterator

--- inc/text_blob_read_iterator.h
+++ inc/text_blob_read_iterator.h
@@ -15,7 +15,7 @@ struct text_blob_read_iterator
 	user_value prefix;
 	uint32_t bytes_read_from_prefix;
 
-	// head_page_id to start the worm
+	// head_page_id to start the worm, unused if is_short is set
 	uint64_t extension_head_page_id;
 
 	// above three attributes are cached in the constructor
@@ -28,7 +28,7 @@ struct text_blob_read_iterator
 	const page_access_methods* pam_p;
 };
 
-text_blob_read_iterator* get_new_text_blob_read_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_data_accessor, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p);
+text_blob_read_iterator* get_new_text_blob_read_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p);
 
 void delete_text_blob_read_iterator(text_blob_read_iterator* tbri_p, const void* transaction_id, int* abort_error);
 
--- src/text_blob_read_iterator.c
+++ src/text_blob_read_iterator.c
@@ -2,7 +2,46 @@
 
 #include<stdlib.h>
 
-text_blob_read_iterator* get_new_text_blob_read_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_data_accessor, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p);
+text_blob_read_iterator* get_new_text_blob_read_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p)
+{
+	text_blob_read_iterator* tbri_p = malloc(sizeof(text_blob_read_iterator));
+	if(tbri_p == NULL)
+		exit(-1);
+
+	const data_type_info* dti_p = get_type_info_for_element_from_tuple_def(tpl_d, inline_accessor);
+	tbri_p->is_short = is_text_short_type_info(dti_p) || is_blob_short_type_info(dti_p);
+
+	if(tbri_p->is_short)
+		get_value_from_element_from_tuple(&(tbri_p->prefix), tpl_d, inline_accessor, tupl);
+	else
+	{
+		positional_accessor child_inline_accessor = {.positions_length = 0, .positions = malloc(sizeof(uint32_t) * (inline_accessor.positions_length + 1))};
+		if(child_inline_accessor.positions == NULL)
+			exit(-1);
+		append_positions(&child_inline_accessor, inline_accessor);
+
+		append_positions(&child_inline_accessor, STATIC_POSITION(0));
+		get_value_from_element_from_tuple(&(tbri_p->prefix), tpl_d, child_inline_accessor, tupl);
+		child_inline_accessor.positions_length--;
+
+		user_value worm_head_page_id;
+		append_positions(&child_inline_accessor, STATIC_POSITION(1));
+		get_value_from_element_from_tuple(&worm_head_page_id, tpl_d, child_inline_accessor, tupl);
+		child_inline_accessor.positions_length--;
+		tbri_p->extension_head_page_id = worm_head_page_id.uint_value;
+
+		free(child_inline_accessor.positions);
+	}
+
+	tbri_p->bytes_read_from_prefix = 0;
+
+	tbri_p->wri = NULL;
+
+	tbri_p->wtd_p = wtd_p;
+	tbri_p->pam_p = pam_p;
+
+	return tbri_p;
+}
 
 void delete_text_blob_read_iterator(text_blob_read_iterator* tbri_p, const void* transaction_id, int* abort_error)
 {
