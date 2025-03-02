76a922e0c2e85ceab9eaa532c658c6fd243b5f28
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-02 21:08:32 +0530

    started with implementation

--- src/text_blob_read_iterator.c
+++ src/text_blob_read_iterator.c
@@ -1,1 +1,9 @@
 #include<text_blob_read_iterator.h>
+
+text_blob_read_iterator* get_new_text_blob_read_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_data_accessor, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p);
+
+void delete_text_blob_read_iterator(text_blob_read_iterator* tbri, const void* transaction_id, int* abort_error);
+
+text_blob_read_iterator* clone_text_blob_read_iterator(text_blob_read_iterator* tbri, const void* transaction_id, int* abort_error);
+
+uint32_t read_from_text_blob(text_blob_read_iterator* tbri, void* data, uint32_t data_size, const void* transaction_id, int* abort_error);
--- src/text_blob_write_iterator.c
+++ src/text_blob_write_iterator.c
@@ -1,1 +1,7 @@
 #include<text_blob_write_iterator.h>
+
+text_blob_write_iterator* get_new_text_blob_write_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t bytes_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p);
+
+void delete_text_blob_write_iterator(text_blob_write_iterator* tbwi, const void* transaction_id, int* abort_error);
+
+uint32_t append_to_text_blob(text_blob_write_iterator* tbwi, const char* data, uint32_t data_size, const void* transaction_id, int* abort_error);
