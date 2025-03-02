080c364339800b59a820fb7beaece0f8d97e4bf6
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-02 21:05:32 +0530

    minor function name change

--- inc/text_blob_read_iterator.h
+++ inc/text_blob_read_iterator.h
@@ -30,7 +30,7 @@ text_blob_read_iterator* get_new_text_blob_read_iterator(void* tupl, tuple_def* 
 
 void delete_text_blob_read_iterator(text_blob_read_iterator* tbri, const void* transaction_id, int* abort_error);
 
-uint32_t read_text_blob_read_iterator(text_blob_read_iterator* tbri, void* data, uint32_t data_size, const void* transaction_id, int* abort_error);
+uint32_t read_from_text_blob(text_blob_read_iterator* tbri, void* data, uint32_t data_size, const void* transaction_id, int* abort_error);
 
 text_blob_read_iterator* clone_text_blob_read_iterator(text_blob_read_iterator* tbri, const void* transaction_id, int* abort_error);
 
