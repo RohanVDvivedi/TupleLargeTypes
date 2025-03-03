602de70b5ebc7d4f8bc72a568a27c389f5b37548
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-03 20:08:23 +0530

    reading from worm for text blob read iterator implemented

--- src/text_blob_read_iterator.c
+++ src/text_blob_read_iterator.c
@@ -1,5 +1,7 @@
 #include<text_blob_read_iterator.h>
 
+#include<page_access_methods.h>
+
 #include<stdlib.h>
 
 text_blob_read_iterator* get_new_text_blob_read_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p)
@@ -76,5 +78,51 @@ uint32_t read_from_text_blob(text_blob_read_iterator* tbri_p, void* data, uint32
 	if(data_size == 0)
 		return 0;
 
-	// TODO
+	uint32_t bytes_read = 0;
+
+	while(data_size > 0)
+	{
+		uint32_t bytes_read_this_iteration = 0;
+
+		if(tbri_p->bytes_read_from_prefix < tbri_p->prefix.string_or_blob_size)
+		{
+			bytes_read_this_iteration = min(data_size, tbri_p->prefix.string_or_blob_size - tbri_p->bytes_read_from_prefix);
+			if(data != NULL)
+				memory_move(data, tbri_p->prefix.string_or_blob_value + tbri_p->bytes_read_from_prefix, bytes_read_this_iteration);
+			tbri_p->bytes_read_from_prefix += bytes_read_this_iteration;
+		}
+		else if(!(tbri_p->is_short)) // go here only if it is a large text or blob
+		{
+			// initialize worm read iterator if not done already
+			if(tbri_p->wri_p == NULL)
+			{
+				if(tbri_p->extension_head_page_id == tbri_p->pam_p->pas.NULL_PAGE_ID)
+					goto NOTHING_TO_READ;
+
+				tbri_p->wri_p = get_new_worm_read_iterator(tbri_p->extension_head_page_id, tbri_p->wtd_p, tbri_p->pam_p, transaction_id, abort_error);
+				if(*abort_error) // on abort error, do nothing
+					return 0;
+			}
+
+			bytes_read_this_iteration = read_from_worm(tbri_p->wri_p, data, data_size, transaction_id, abort_error);
+			if(*abort_error) // on abort error, delete worm iterator and set it to NULL
+			{
+				delete_worm_read_iterator(tbri_p->wri_p, transaction_id, abort_error);
+				tbri_p->wri_p = NULL;
+				return 0;
+			}
+		}
+
+		// skip label to goto, if nothing to be read is found
+		NOTHING_TO_READ:;
+
+		if(bytes_read_this_iteration == 0)
+			break;
+
+		data += bytes_read_this_iteration;
+		data_size -= bytes_read_this_iteration;
+		bytes_read += bytes_read_this_iteration;
+	}
+
+	return bytes_read;
 }
