99a7873c8291a639d0089e6832b3c6d9202cb15c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-04 14:03:17 +0530

    basic record def initialization implemented

--- test/test.c
+++ test/test.c
@@ -4,8 +4,8 @@
 #include<blob_large.h>
 #include<text_large.h>
 
-#define USE_TEXT
-//#define USE_BLOB
+#define USE_SHORT
+//#define USE_LARGE
 
 #define USE_BASE
 //#define USE_NESTED
@@ -15,6 +15,7 @@
 #else
 	#define ACCS STATIC_POSITION(0)
 #endif
+// use ACCS as accessor to access the attribute
 
 #define PREFIX_SIZE 15
 
@@ -42,9 +43,36 @@ char* test_data = "Rohan is a good boy,"
 					"I will probably change it to lorem ipsum once this project goes big enough";
 
 tuple_def tpl_d;
-tuple_def* get_tuple_definition()
+data_type_info* short_dti = NULL;
+data_type_info* large_dti = NULL;
+char tuple_type_info_memory[sizeof_tuple_data_type_info(2)];
+data_type_info* tuple_dti = tuple_type_info_memory;
+tuple_def* get_tuple_definition(const page_access_specs* pas_p)
 {
-	return NULL;
+	short_dti = get_text_short_type_info(PREFIX_SIZE + 10);
+	large_dti = get_text_large_type_info(short_dti, pas_p);
+
+	initialize_tuple_data_type_info(tuple_dti, "container", 1, PAGE_SIZE, 1);
+	strcpy(tuple_dti->containees[0].field_name, "containee");
+	#ifdef USE_SHORT
+		tuple_dti->containees[0].al.type_info = short_dti;
+	#else
+		tuple_dti->containees[0].al.type_info = large_dti;
+	#endif
+
+	data_type_info* dti = NULL;
+	#ifdef USE_NESTED
+		dti = tuple_dti;
+	#elif defined USE_BASE
+		#ifdef USE_SHORT
+			dti = short_dti;
+		#elif defined USE_LARGE
+			dti = large_dti;
+		#endif
+	#endif
+
+	initialize_tuple_def(&tpl_d, dti);
+	return &tpl_d;
 }
 
 void insert_all_test_data(tuple_def* tpl_d, char* inline_tuple, worm_tuple_defs* wtd_p, page_access_methods* pam_p, page_modification_methods* pmm_p)
@@ -106,6 +134,8 @@ int main()
 	delete_unWALed_page_modification_methods(pmm_p);
 
 	// delete the record definition
+	free(short_dti);
+	free(large_dti);
 
 	return 0;
 }
