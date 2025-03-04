86e1fc270233b7dc4bb1b5f92e24b8be904861cf
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-04 15:07:44 +0530

    if there is a worm in the large text of blob then never append to the prefix even if the user wants us to

--- src/text_blob_write_iterator.c
+++ src/text_blob_write_iterator.c
@@ -3,38 +3,6 @@
 #include<stdlib.h>
 #include<page_access_methods.h>
 
-text_blob_write_iterator* get_new_text_blob_write_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t bytes_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p)
-{
-	text_blob_write_iterator* tbwi_p = malloc(sizeof(text_blob_write_iterator));
-	if(tbwi_p == NULL)
-		exit(-1);
-
-	const data_type_info* dti_p = get_type_info_for_element_from_tuple_def(tpl_d, inline_accessor);
-	tbwi_p->is_short = is_text_short_type_info(dti_p) || is_blob_short_type_info(dti_p);
-
-	tbwi_p->tupl = tupl;
-	tbwi_p->tpl_d = tpl_d;
-	tbwi_p->inline_accessor = inline_accessor;
-
-	tbwi_p->bytes_to_be_written_to_prefix = bytes_to_be_written_to_prefix;
-	tbwi_p->bytes_written_to_prefix = 0;
-
-	tbwi_p->wai_p = NULL;
-
-	tbwi_p->wtd_p = wtd_p;
-	tbwi_p->pam_p = pam_p;
-	tbwi_p->pmm_p = pmm_p;
-
-	return tbwi_p;
-}
-
-void delete_text_blob_write_iterator(text_blob_write_iterator* tbwi_p, const void* transaction_id, int* abort_error)
-{
-	if(tbwi_p->wai_p != NULL)
-		delete_worm_append_iterator(tbwi_p->wai_p, transaction_id, abort_error);
-	free(tbwi_p);
-}
-
 static inline positional_accessor initialize_attribute_accessor(text_blob_write_iterator* tbwi_p)
 {
 	positional_accessor pa = {.positions_length = 0, .positions = malloc(sizeof(uint32_t) * (tbwi_p->inline_accessor.positions_length + 2))};
@@ -86,6 +54,53 @@ static inline void point_to_extension_head_page_id(text_blob_write_iterator* tbw
 static inline void deinitialize_attribute_accessor(text_blob_write_iterator* tbwi_p, positional_accessor* pa)
 {
 	free(pa->positions);
+}
+
+text_blob_write_iterator* get_new_text_blob_write_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t bytes_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p)
+{
+	text_blob_write_iterator* tbwi_p = malloc(sizeof(text_blob_write_iterator));
+	if(tbwi_p == NULL)
+		exit(-1);
+
+	const data_type_info* dti_p = get_type_info_for_element_from_tuple_def(tpl_d, inline_accessor);
+	tbwi_p->is_short = is_text_short_type_info(dti_p) || is_blob_short_type_info(dti_p);
+
+	tbwi_p->tupl = tupl;
+	tbwi_p->tpl_d = tpl_d;
+	tbwi_p->inline_accessor = inline_accessor;
+
+	// if it is a large_string or large_blob, with a worm attached then make bytes_to_be_written_to_prefix = 0
+	if(!(tbwi_p->is_short))
+	{
+		positional_accessor child_inline_accessor = initialize_attribute_accessor(tbwi_p);
+
+		point_to_extension_head_page_id(tbwi_p, &child_inline_accessor);
+
+		user_value extension_head_page;
+		int valid = get_value_from_element_from_tuple(&extension_head_page, tbwi_p->tpl_d, child_inline_accessor, tbwi_p->tupl);
+		if(valid && !is_user_value_NULL(extension_head_page) && extension_head_page.uint_value != tbwi_p->pam_p->pas.NULL_PAGE_ID)
+			bytes_to_be_written_to_prefix = 0;
+
+		deinitialize_attribute_accessor(tbwi_p, &child_inline_accessor);
+	}
+
+	tbwi_p->bytes_to_be_written_to_prefix = bytes_to_be_written_to_prefix;
+	tbwi_p->bytes_written_to_prefix = 0;
+
+	tbwi_p->wai_p = NULL;
+
+	tbwi_p->wtd_p = wtd_p;
+	tbwi_p->pam_p = pam_p;
+	tbwi_p->pmm_p = pmm_p;
+
+	return tbwi_p;
+}
+
+void delete_text_blob_write_iterator(text_blob_write_iterator* tbwi_p, const void* transaction_id, int* abort_error)
+{
+	if(tbwi_p->wai_p != NULL)
+		delete_worm_append_iterator(tbwi_p->wai_p, transaction_id, abort_error);
+	free(tbwi_p);
 }
 
 uint32_t append_to_text_blob(text_blob_write_iterator* tbwi_p, const char* data, uint32_t data_size, const void* transaction_id, int* abort_error)
