8125d499c0fad9801c5322628ef096d501832509
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-08 22:45:45 +0530

    added more clarification for the large numeric type

--- inc/numeric_large.h
+++ inc/numeric_large.h
@@ -22,9 +22,10 @@ data_type_info* get_numeric_large_type_info(const data_type_info* numeric_short_
 	SHORT NUMERIC TYPE
 	numeric type here is represented as a base 10^12 number, with each digit being a 5 byte unsigned number between [0, 10^12), in-memory representation of each digit will be a uint64_t
 	each shot numeric type is composed of
-	  * 2 sign bits ==> 00 -> -infinity, 01 -> negative number, 10 -> positive number and 11 -> +infinity
-	  * 2 byte exponent
-	  * some N number of digits as an array of 5 byte unsigned integers
+		* 2 sign bits ==> 00 -> -infinity, 01 -> negative number, 10 -> positive number and 11 -> +infinity
+		* 2 byte exponent
+		* some N number of digits as an array of 5 byte unsigned integers
+
 	Explanation of choosing a 5 byte integer to store 12 decimal digits of information comes from the following
 		* storing mantissa is a big problem, we need to store digit groups as unsigned integers
 		* an X byte big unsigned integer can store Y digits, then Y = log(256 ^ X) / log(10) = X * 8 * log(2) / log(10)
@@ -42,4 +43,8 @@ data_type_info* get_numeric_large_type_info(const data_type_info* numeric_short_
 		* first task is to build a function to set one of the 12 digits in a uint64_t
 		* now since we are storing digits in base 10^12 in a 5 byte integer, why not store exponent represented as a (10^12) ^ exponent
 		* now everything becomes radix 10^12 instead of radix of 10, and with 2 sign bits, 16 bit exponent and finally an array of 5 byte integers, each storing precisely 12 digits worth of information as a 10^12 radix digit
+
+	LARGE NUMERIC TYPE
+	will just be a tuple of a short numeric type and a page_id to point to a worm
+	any and all following digit that could not be accomodated in the prefix will be pushed into the worm in 5 byte chunks one after the another
 */
