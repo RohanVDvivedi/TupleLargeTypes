4b36d0a88a4edea6ad0c8194644614f928761081
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-09 00:11:36 +0530

    now you can easily delete the large types by getting their head_page_id

--- test/test.c
+++ test/test.c
@@ -179,7 +179,14 @@ int main()
 	/* CLEANUP */
 
 	// destroy worm
-	// TODO
+	uint64_t head_page_id = get_extension_head_page_id_for_large_type(inline_tuple, tpl_d, ACCS, &(pam_p->pas));
+	uint64_t dependent_root_page_id;
+	int vaccum_needed = 0;
+	if(head_page_id != pam_p->pas.NULL_PAGE_ID)
+	{
+		decrement_reference_counter_for_worm(head_page_id, &dependent_root_page_id, &vaccum_needed, &wtd, pam_p, pmm_p, transaction_id, &abort_error);
+		printf("dependent_root_page_id = %"PRIu64" vaccum_needed = %d\n", dependent_root_page_id, vaccum_needed);
+	}
 
 	// close the in-memory data store
 	close_and_destroy_unWALed_in_memory_data_store(pam_p);
