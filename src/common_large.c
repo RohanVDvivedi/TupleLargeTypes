f56bc5ece1e946a47f648eab923fd05a17f367ad
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-09 23:21:37 +0530

    making the common large header api work using the relative position accessor

--- src/common_large.c
+++ src/common_large.c
@@ -2,6 +2,8 @@
 
 #include<stdlib.h>
 
+#include<relative_positional_accessor.h>
+
 int is_short_type_info(const data_type_info* dti_p)
 {
 	int type_name_length = strnlen(dti_p->type_name, sizeof(dti_p->type_name));
@@ -29,34 +31,31 @@ uint64_t get_extension_head_page_id_for_large_type(const void* tupl, const tuple
 			return pas_p->NULL_PAGE_ID;
 	}
 
-	positional_accessor pa_temp = {.positions_length = 0, .positions = malloc(sizeof(uint32_t) * pa.positions_length)};
-	if(pa_temp.positions == NULL)
-		exit(-1);
-	append_positions(&pa_temp, pa);
+	relative_positional_accessor rpa;
+	initialize_relative_positional_accessor(&rpa, &pa, 1);
 
 	// before a worm is created and appended to, the prefix has to be non null and valid, (it may not be full, but it must have something)
 	{
 		//pa_temp.positions_length = pa.positions_length;
-		append_positions(&pa_temp, STATIC_POSITION(0));
+		relative_positonal_accessor_set_from_relative(&rpa, STATIC_POSITION(0));
 		user_value prefix;
-		int valid = get_value_from_element_from_tuple(&prefix, tpl_d, pa_temp, tupl);
+		int valid = get_value_from_element_from_tuple(&prefix, tpl_d, rpa.exact, tupl);
 		if((!valid) || is_user_value_NULL(&prefix))
 		{
-			free(pa_temp.positions);
+			deinitialize_relative_positional_accessor(&rpa);
 			return pas_p->NULL_PAGE_ID;
 		}
 	}
 
 	uint64_t extension_head_page_id = pas_p->NULL_PAGE_ID;
 	{
-		pa_temp.positions_length = pa.positions_length;
-		append_positions(&pa_temp, STATIC_POSITION(1));
+		relative_positonal_accessor_set_from_relative(&rpa, STATIC_POSITION(1));
 		user_value extension_head_page;
-		int valid = get_value_from_element_from_tuple(&extension_head_page, tpl_d, pa_temp, tupl);
+		int valid = get_value_from_element_from_tuple(&extension_head_page, tpl_d, rpa.exact, tupl);
 		if(valid && (!is_user_value_NULL(&extension_head_page)))
 			extension_head_page_id = extension_head_page.uint_value;
 	}
 
-	free(pa_temp.positions);
+	deinitialize_relative_positional_accessor(&rpa);
 	return extension_head_page_id;
 }
