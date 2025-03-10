a8521567ec6d0a081bdc7fd8c163b5b495d2c1ee
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-10 21:37:25 +0530

    added macros for evaluating sign bits

--- inc/numeric_large.h
+++ inc/numeric_large.h
@@ -27,6 +27,9 @@ enum numeric_sign_bits
 	POSITIVE_NUMERIC = 2,
 	POSITIVE_INFINITY_NUMERIC = 3,
 };
+#define IF_INFINITY_NUMERIC_SIGN_BIT(s) (((s) == NEGATIVE_INFINITY_NUMERIC) || ((s) == POSITIVE_INFINITY_NUMERIC))
+#define IF_NEGATIVE_NUMERIC_SIGN_BIT(s) (((s) <= NEGATIVE_NUMERIC))
+#define IF_POSITIVE_NUMERIC_SIGN_BIT(s) (((s) >= POSITIVE_NUMERIC))
 
 // pass in/out parameter sign_bits and exponent based on what ever you need
 int extract_sign_bits_and_exponent_from_numeric(numeric_sign_bits* sign_bits, int16_t* exponent, const void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor);
@@ -34,7 +37,7 @@ int extract_sign_bits_and_exponent_from_numeric(numeric_sign_bits* sign_bits, in
 // below functions can be used to compare numeric prefix using just sign_bits (s1 and s2) and exponent (e1 and e2), without consulting the digits
 // a flag digits_required will be set if the further comparison of digits will be necessary
 // NOTE: this functions assumes that the numeric is stored in correct scientific notation of base 10^12, as explained in the large comment at the end of this file
-// NOTE: this functions assumes that 0 numeric is always encoded as POSITIVE_NUMERIC, exp = 0, digits = 0...atleast 1 0
+// NOTE: this functions assumes that 0 numeric is always encoded as POSITIVE_NUMERIC, exp = 0, digits = 0
 // if digits_required is 1 after the call to this function, then you need to complete the comparison with comparison of digits
 int compare_numeric_prefix_no_digits(numeric_sign_bits s1, int16_t e1, numeric_sign_bits s2, int16_t e2, int* digits_required);
 
