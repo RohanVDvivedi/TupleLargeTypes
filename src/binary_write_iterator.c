3fe304eb484eb3b3f08e1c93461d96526bb1a078
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-11 22:00:15 +0530

    initialization is done just once for the binary write iterator

--- src/binary_write_iterator.c
+++ src/binary_write_iterator.c
@@ -40,7 +40,7 @@ static inline void point_to_extension_head_page_id(relative_positional_accessor*
 
 	relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(1));
 }
-#include<stdio.h>
+
 binary_write_iterator* get_new_binary_write_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t bytes_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p)
 {
 	binary_write_iterator* bwi_p = malloc(sizeof(binary_write_iterator));
@@ -54,12 +54,12 @@ binary_write_iterator* get_new_binary_write_iterator(void* tupl, tuple_def* tpl_
 	bwi_p->tpl_d = tpl_d;
 	bwi_p->inline_accessor = inline_accessor;
 
+	relative_positional_accessor child_relative_accessor;
+	initialize_relative_positional_accessor(&child_relative_accessor, &(bwi_p->inline_accessor),2);
+
 	// if it is a large_string or large_blob, with a worm attached then make bytes_to_be_written_to_prefix = 0
 	if(!(bwi_p->is_inline))
 	{
-		relative_positional_accessor child_relative_accessor;
-		initialize_relative_positional_accessor(&child_relative_accessor, &(bwi_p->inline_accessor), 2);
-
 		point_to_extension_head_page_id(&child_relative_accessor, bwi_p->is_inline);
 		user_value extension_head_page;
 		int valid_extension = get_value_from_element_from_tuple(&extension_head_page, bwi_p->tpl_d, child_relative_accessor.exact, bwi_p->tupl);
@@ -70,11 +70,9 @@ binary_write_iterator* get_new_binary_write_iterator(void* tupl, tuple_def* tpl_
 
 		if(valid_extension && valid_prefix && !is_user_value_NULL(&extension_head_page) && !is_user_value_NULL(&prefix) && extension_head_page.uint_value != pam_p->pas.NULL_PAGE_ID)
 			bytes_to_be_written_to_prefix = 0;
-
-		deinitialize_relative_positional_accessor(&child_relative_accessor);
 	}
 
-	bwi_p->bytes_to_be_written_to_prefix = bytes_to_be_written_to_prefix; printf("=>%u\n", bwi_p->bytes_to_be_written_to_prefix);
+	bwi_p->bytes_to_be_written_to_prefix = bytes_to_be_written_to_prefix;
 	bwi_p->bytes_written_to_prefix = 0;
 
 	bwi_p->wai_p = NULL;
@@ -83,30 +81,18 @@ binary_write_iterator* get_new_binary_write_iterator(void* tupl, tuple_def* tpl_
 	bwi_p->pam_p = pam_p;
 	bwi_p->pmm_p = pmm_p;
 
-	return bwi_p;
-}
-
-void delete_binary_write_iterator(binary_write_iterator* bwi_p, const void* transaction_id, int* abort_error)
-{
-	if(bwi_p->wai_p != NULL)
-		delete_worm_append_iterator(bwi_p->wai_p, transaction_id, abort_error);
-	free(bwi_p);
-}
-
-uint32_t append_to_binary_write_iterator(binary_write_iterator* bwi_p, const char* data, uint32_t data_size, const void* transaction_id, int* abort_error)
-{
-	if(data_size == 0)
-		return 0;
-
-	relative_positional_accessor child_relative_accessor;
-	initialize_relative_positional_accessor(&child_relative_accessor, &(bwi_p->inline_accessor), 2);
-
 	// initialization
 	// if the attribute is NULL, set it to EMPTY_USER_VALUE
 	{
 		point_to_attribute(&child_relative_accessor, bwi_p->is_inline);
 		user_value attr;
-		get_value_from_element_from_tuple(&attr, bwi_p->tpl_d, child_relative_accessor.exact, bwi_p->tupl);
+		int valid_attr = get_value_from_element_from_tuple(&attr, bwi_p->tpl_d, child_relative_accessor.exact, bwi_p->tupl);
+		if(!valid_attr)
+		{
+			free(bwi_p);
+			deinitialize_relative_positional_accessor(&child_relative_accessor);
+			return NULL;
+		}
 		if(is_user_value_NULL(&attr))
 			set_element_in_tuple(bwi_p->tpl_d, child_relative_accessor.exact, bwi_p->tupl, EMPTY_USER_VALUE, UINT32_MAX);
 	}
@@ -135,8 +121,26 @@ uint32_t append_to_binary_write_iterator(binary_write_iterator* bwi_p, const cha
 	{
 		point_to_attribute(&child_relative_accessor, bwi_p->is_inline);
 		bwi_p->bytes_to_be_written_to_prefix = min(bwi_p->bytes_to_be_written_to_prefix, get_max_size_increment_allowed_for_element_in_tuple(bwi_p->tpl_d, child_relative_accessor.exact, bwi_p->tupl));
-		printf("->%u\n", bwi_p->bytes_to_be_written_to_prefix);
 	}
+
+	deinitialize_relative_positional_accessor(&child_relative_accessor);
+	return bwi_p;
+}
+
+void delete_binary_write_iterator(binary_write_iterator* bwi_p, const void* transaction_id, int* abort_error)
+{
+	if(bwi_p->wai_p != NULL)
+		delete_worm_append_iterator(bwi_p->wai_p, transaction_id, abort_error);
+	free(bwi_p);
+}
+
+uint32_t append_to_binary_write_iterator(binary_write_iterator* bwi_p, const char* data, uint32_t data_size, const void* transaction_id, int* abort_error)
+{
+	if(data_size == 0)
+		return 0;
+
+	relative_positional_accessor child_relative_accessor;
+	initialize_relative_positional_accessor(&child_relative_accessor, &(bwi_p->inline_accessor), 2);
 
 	uint32_t bytes_written = 0;
 
