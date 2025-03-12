09d6bbee5360e0f3198d2df5f0e711c517d35fe9
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-12 22:45:37 +0530

    added function to set sign bits and exponent to the numeric type

--- src/numeric_extended.c
+++ src/numeric_extended.c
@@ -115,6 +115,68 @@ int extract_sign_bits_and_exponent_from_numeric(numeric_sign_bits* sign_bits, in
 	return result;
 }
 
+int set_sign_bits_and_exponent_for_numeric(const numeric_sign_bits* sign_bits, const int16_t* exponent, void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor)
+{
+	int is_extended = 0;
+	{
+		const data_type_info* dti_p = get_type_info_for_element_from_tuple_def(tpl_d, inline_accessor);
+		if(is_numeric_extended_type_info(dti_p))
+			is_extended = 1;
+		else if(is_numeric_inline_type_info(dti_p))
+			is_extended = 0;
+		else
+			return 0;
+	}
+
+	// why call this function if you do not need to set any thing
+	if(sign_bits == NULL && exponent == NULL)
+		return 1;
+
+	relative_positional_accessor rpa;
+	initialize_relative_positional_accessor(&rpa, &inline_accessor, 2);
+
+	// if it is extended type, then we need to make sure that the 0th element inside it, the inline component is not NULL
+	if(is_extended)
+	{
+		relative_positonal_accessor_set_from_relative(&rpa, STATIC_POSITION(0));
+		user_value inline_component;
+		int valid = get_value_from_element_from_tuple(&inline_component, tpl_d, rpa.exact, tupl);
+		if(!valid) // this should not happen, for most cases
+		{
+			deinitialize_relative_positional_accessor(&rpa);
+			return 0;
+		}
+		if(is_user_value_NULL(&inline_component)) // set it to empty user value only if it is absent, else tht set calls to its sign bits and exponent will fail
+			set_element_in_tuple(tpl_d, rpa.exact, tupl, EMPTY_USER_VALUE, 0);
+	}
+
+	int result = 1;
+
+	// set sign_bits
+	if(sign_bits != NULL && result == 1)
+	{
+		if(is_extended)
+			relative_positonal_accessor_set_from_relative(&rpa, STATIC_POSITION(0, 0));
+		else
+			relative_positonal_accessor_set_from_relative(&rpa, STATIC_POSITION(0));
+		result = result && set_element_in_tuple(tpl_d, rpa.exact, tupl, &((user_value){.bit_field_value = (*sign_bits)}), 0);
+	}
+
+	// set exponent
+	if(exponent != NULL && result == 1)
+	{
+		if(is_extended)
+			relative_positonal_accessor_set_from_relative(&rpa, STATIC_POSITION(0, 1));
+		else
+			relative_positonal_accessor_set_from_relative(&rpa, STATIC_POSITION(1));
+		result = result && set_element_in_tuple(tpl_d, rpa.exact, tupl, &((user_value){.int_value = (*exponent)}), 0);
+	}
+
+	deinitialize_relative_positional_accessor(&rpa);
+
+	return result;
+}
+
 int compare_numeric_prefix_no_digits(numeric_sign_bits s1, int16_t e1, numeric_sign_bits s2, int16_t e2, int* digits_requirement)
 {
 	// start with comparing the sign bits
