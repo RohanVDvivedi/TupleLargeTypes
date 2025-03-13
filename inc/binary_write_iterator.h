b8698780bb1569e3b9b7e1ad0c26a225f334c83c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-13 23:40:09 +0530

    minor comment changes

--- inc/binary_write_iterator.h
+++ inc/binary_write_iterator.h
@@ -43,7 +43,7 @@ struct binary_write_iterator
 	suffix is transaction protected, and could be persistent
 */
 
-// the text of blob in context must be either NULL or atleast EMPTY
+// the text or blob in context must be EMPTY_USER_VALUE or partially written
 binary_write_iterator* get_new_binary_write_iterator(void* tupl, const tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t bytes_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p);
 
 void delete_binary_write_iterator(binary_write_iterator* bwi_p, const void* transaction_id, int* abort_error);
