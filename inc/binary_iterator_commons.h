144f577a3fa2121953f1e1eafe7faf3cea13ef93
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-13 14:27:39 +0530

    moving common code to a separate file

---
+++ inc/binary_iterator_commons.h
@@ -0,0 +1,39 @@
+#ifndef BINARY_ITERATOR_COMMONS_H
+#define BINARY_ITERATOR_COMMONS_H
+
+static inline void point_to_attribute(relative_positional_accessor* rpa, int is_inline)
+{
+	relative_positonal_accessor_set_from_relative(rpa, SELF);
+}
+
+static inline void point_to_prefix(relative_positional_accessor* rpa, int is_inline)
+{
+	if(is_inline)
+	{
+		relative_positonal_accessor_set_from_relative(rpa, SELF);
+		return ;
+	}
+
+	relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(0));
+}
+
+static inline void point_to_prefix_s_byte(relative_positional_accessor* rpa, uint32_t index, int is_inline)
+{
+	if(is_inline)
+	{
+		relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(index));
+		return ;
+	}
+
+	relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(0, index));
+}
+
+static inline void point_to_extension_head_page_id(relative_positional_accessor* rpa, int is_inline)
+{
+	if(is_inline)
+		return ;
+
+	relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(1));
+}
+
+#endif
--- src/binary_write_iterator.c
+++ src/binary_write_iterator.c
@@ -6,40 +6,7 @@
 
 #include<relative_positional_accessor.h>
 
-static inline void point_to_attribute(relative_positional_accessor* rpa, int is_inline)
-{
-	relative_positonal_accessor_set_from_relative(rpa, SELF);
-}
-
-static inline void point_to_prefix(relative_positional_accessor* rpa, int is_inline)
-{
-	if(is_inline)
-	{
-		relative_positonal_accessor_set_from_relative(rpa, SELF);
-		return ;
-	}
-
-	relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(0));
-}
-
-static inline void point_to_prefix_character(relative_positional_accessor* rpa, uint32_t index, int is_inline)
-{
-	if(is_inline)
-	{
-		relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(index));
-		return ;
-	}
-
-	relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(0, index));
-}
-
-static inline void point_to_extension_head_page_id(relative_positional_accessor* rpa, int is_inline)
-{
-	if(is_inline)
-		return ;
-
-	relative_positonal_accessor_set_from_relative(rpa, STATIC_POSITION(1));
-}
+#include<binary_iterator_commons.h>
 
 binary_write_iterator* get_new_binary_write_iterator(void* tupl, tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t bytes_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p)
 {
@@ -160,7 +127,7 @@ uint32_t append_to_binary_write_iterator(binary_write_iterator* bwi_p, const cha
 			// copy data into it byte by byte
 			for(uint32_t i = 0; i < bytes_written_this_iteration; i++)
 			{
-				point_to_prefix_character(&child_relative_accessor, old_element_count + i, bwi_p->is_inline);
+				point_to_prefix_s_byte(&child_relative_accessor, old_element_count + i, bwi_p->is_inline);
 				set_element_in_tuple(bwi_p->tpl_d, child_relative_accessor.exact, bwi_p->tupl, &((user_value){.uint_value = data[i]}), UINT32_MAX);
 			}
 
