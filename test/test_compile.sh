bfba168cee1d78d4852c1b650fc75965e90f4f40
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-17 01:26:53 +0530

    enabling printing all the tests

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,3 @@
-#gcc ./test_text_blob.c -o test_tb.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
-#gcc ./test_numeric.c -o test_n.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
+gcc ./test_text_blob.c -o test_tb.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
+gcc ./test_numeric.c -o test_n.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
 gcc ./test_tuple_list.c -o test_tl.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
