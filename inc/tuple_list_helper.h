d95194c60a50738baed68fa0287ee9939217d1be
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-16 23:24:02 +0530

    added new function declarations for the tuple list

---
+++ inc/tuple_list_helper.h
@@ -0,0 +1,12 @@
+#ifndef TUPLE_LIST_HELPER_H
+#define TUPLE_LIST_HELPER_H
+
+#include<binary_read_iterator.h>
+
+// returns 1 if a tuple was skipped, else returns 0 for abort_error OR end of data
+int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error);
+
+// returns a malloc-ed point to a next tuple read from the iterator, else returns NULL for abort_error OR end of data
+void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error);
+
+#endif
---
+++ src/tuple_list_helper.c
@@ -0,0 +1,11 @@
+#include<tuple_list_helper.h>
+
+int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error)
+{
+	// TODO
+}
+
+void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error)
+{
+	// TODO
+}
