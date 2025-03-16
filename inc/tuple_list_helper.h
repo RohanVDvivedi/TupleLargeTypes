e673ded0df2dc4f22fb1d470775b97cd8c5e0a29
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-16 23:50:54 +0530

    added helper functions to decipher tuples from the tuple_list

--- inc/tuple_list_helper.h
+++ inc/tuple_list_helper.h
@@ -4,9 +4,9 @@
 #include<binary_read_iterator.h>
 
 // returns 1 if a tuple was skipped, else returns 0 for abort_error OR end of data
-int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error);
+int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
 
 // returns a malloc-ed point to a next tuple read from the iterator, else returns NULL for abort_error OR end of data
-void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error);
+void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
 
 #endif
--- src/tuple_list_helper.c
+++ src/tuple_list_helper.c
@@ -2,17 +2,32 @@
 
 #include<stdlib.h>
 
-// the passed buffer must be atleast 4 bytes big, buffer_size will be set to bytes read from the bri_p
-static uint32_t get_next_tuple_size_from_binary_read_iterator(binary_read_iterator* bri_p, char* buffer, uint32_t* buffer_size, const void* transaction_id, int* abort_error)
+typedef struct get_size_context get_size_context;
+struct get_size_context
 {
-	// TODO
+	binary_read_iterator* bri_p;
+	const void* transaction_id;
+	int* abort_error;
+};
+
+static uint32_t read_from_stream_for_get_size_context(void* context_p, void* data, uint32_t data_size)
+{
+	get_size_context* c_p = context_p;
+	return read_from_binary_read_iterator(c_p->bri_p, data, data_size, c_p->transaction_id, c_p->abort_error);
 }
 
-int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error)
+// the passed buffer must be atleast 4 bytes big, buffer_size will be set to bytes read from the bri_p
+static uint32_t get_next_tuple_size_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, char* buffer, uint32_t* buffer_size, const void* transaction_id, int* abort_error)
+{
+	(*buffer_size) = 0;
+	return get_tuple_size_from_stream(tpl_d, buffer, buffer_size, &(get_size_context){bri_p, transaction_id, abort_error}, read_from_stream_for_get_size_context);
+}
+
+int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error)
 {
 	char buffer[4];
 	uint32_t bytes_read = 0;
-	const uint32_t bytes_to_read = get_next_tuple_size_from_binary_read_iterator(bri_p, buffer, &bytes_read, transaction_id, abort_error);
+	const uint32_t bytes_to_read = get_next_tuple_size_from_binary_read_iterator(bri_p, tpl_d, buffer, &bytes_read, transaction_id, abort_error);
 	if(*abort_error)
 		return 0;
 
@@ -36,11 +51,11 @@ int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void
 	return 1;
 }
 
-void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const void* transaction_id, int* abort_error)
+void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error)
 {
 	char buffer[4];
 	uint32_t bytes_read = 0;
-	const uint32_t bytes_to_read = get_next_tuple_size_from_binary_read_iterator(bri_p, buffer, &bytes_read, transaction_id, abort_error);
+	const uint32_t bytes_to_read = get_next_tuple_size_from_binary_read_iterator(bri_p, tpl_d, buffer, &bytes_read, transaction_id, abort_error);
 	if(*abort_error)
 		return NULL;
 
