64ffad4e4deffdf47275e8936e37aea200f342bc
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-24 10:13:39 +0530

    added binary hasher interface for the text blob and numeric

---
+++ inc/binary_hasher.h
@@ -0,0 +1,16 @@
+#ifndef BINARY_HASHER_H
+#define BINARY_HASHER_H
+
+#include<tuple_def.h>
+#include<tuple.h>
+
+#include<worm.h>
+#include<page_access_methods.h>
+
+uint64_t hash_tbn(const tuple_def* tpl_d, const void* tupl, positional_accessor inline_accessor, tuple_hasher* th, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+
+#define hash_text hash_tbn
+#define hash_blob hash_tbn
+#define hash_numeric hash_tbn
+
+#endif
---
+++ src/binary_hasher.c
@@ -0,0 +1,6 @@
+#include<binary_hasher.h>
+
+uint64_t hash_tbn(const tuple_def* tpl_d, const void* tupl, positional_accessor inline_accessor, tuple_hasher* th, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
+{
+	
+}
