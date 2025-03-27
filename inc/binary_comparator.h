027589128ad423195d56ad9cfcb683adbf9c1158
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-27 19:41:32 +0530

    added declaration for the compare numeric function

--- inc/binary_comparator.h
+++ inc/binary_comparator.h
@@ -14,6 +14,10 @@ int compare_tb(const tuple_def* tpl_d1, const void* tupl1, positional_accessor i
 #define compare_text compare_tb
 #define compare_blob compare_tb
 
+int compare_numeric(const tuple_def* tpl_d1, const void* tupl1, positional_accessor inline_accessor1, const worm_tuple_defs* wtd1_p, const page_access_methods* pam1_p, const void* transaction_id1, int* abort_error1,
+					const tuple_def* tpl_d2, const void* tupl2, positional_accessor inline_accessor2, const worm_tuple_defs* wtd2_p, const page_access_methods* pam2_p, const void* transaction_id2, int* abort_error2,
+					int* is_prefix);
+
 /*
 	if first parameter is prefix of another then (is_prefix & 1) returns true
 	if second parameter is prefix of another then (is_prefix & 2) returns true
--- src/binary_comparator.c
+++ src/binary_comparator.c
@@ -109,3 +109,10 @@ int compare_tb(const tuple_def* tpl_d1, const void* tupl1, positional_accessor i
 	delete_binary_read_iterator(bri2_p, transaction_id2, abort_error2);
 	return cmp;
 }
+
+int compare_numeric(const tuple_def* tpl_d1, const void* tupl1, positional_accessor inline_accessor1, const worm_tuple_defs* wtd1_p, const page_access_methods* pam1_p, const void* transaction_id1, int* abort_error1,
+					const tuple_def* tpl_d2, const void* tupl2, positional_accessor inline_accessor2, const worm_tuple_defs* wtd2_p, const page_access_methods* pam2_p, const void* transaction_id2, int* abort_error2,
+					int* is_prefix)
+{
+	// TODO
+}
