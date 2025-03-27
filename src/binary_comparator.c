113320dfae20acb452b9a65b859f4af1cd27efc3
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-27 20:05:02 +0530

    comparison using digits implemented

--- src/binary_comparator.c
+++ src/binary_comparator.c
@@ -111,6 +111,7 @@ int compare_tb(const tuple_def* tpl_d1, const void* tupl1, positional_accessor i
 }
 
 #include<numeric_extended.h>
+#include<digit_read_iterator.h>
 
 int compare_numeric(const tuple_def* tpl_d1, const void* tupl1, positional_accessor inline_accessor1, const worm_tuple_defs* wtd1_p, const page_access_methods* pam1_p, const void* transaction_id1, int* abort_error1,
 					const tuple_def* tpl_d2, const void* tupl2, positional_accessor inline_accessor2, const worm_tuple_defs* wtd2_p, const page_access_methods* pam2_p, const void* transaction_id2, int* abort_error2,
@@ -167,9 +168,79 @@ int compare_numeric(const tuple_def* tpl_d1, const void* tupl1, positional_acces
 		}
 	}
 
+
+	// compare using digits
 	int cmp = 0;
 
-	// TODO
+	uint64_t buffer1[BUFFER_CAPACITY/sizeof(uint64_t)];
+	uint64_t buffer2[BUFFER_CAPACITY/sizeof(uint64_t)];
+
+	uint64_t* data1 = buffer1; uint32_t data_size1 = 0;
+	uint64_t* data2 = buffer2; uint32_t data_size2 = 0;
+
+	digit_read_iterator* dri1_p = get_new_digit_read_iterator(tupl1, tpl_d1, inline_accessor1, wtd1_p, pam1_p);
+	digit_read_iterator* dri2_p = get_new_digit_read_iterator(tupl2, tpl_d2, inline_accessor2, wtd2_p, pam2_p);
+
+	int cmp = 0;
+	while(cmp == 0)
+	{
+		if(data_size1 == 0) // if there are no more bytes in the data1 then read it
+		{
+			data1 = buffer1;
+			data_size1 = read_from_digit_read_iterator(dri1_p, data1, BUFFER_CAPACITY/sizeof(uint64_t), transaction_id1, abort_error1);
+			if(*abort_error1)
+			{
+				delete_digit_read_iterator(dri1_p, transaction_id1, abort_error1);
+				delete_digit_read_iterator(dri2_p, transaction_id2, abort_error2);
+				return 0;
+			}
+		}
+
+		if(data_size2 == 0) // if there are no more bytes in the data2 then read it
+		{
+			data2 = buffer2;
+			data_size2 = read_from_digit_read_iterator(dri2_p, data2, BUFFER_CAPACITY/sizeof(uint64_t), transaction_id2, abort_error2);
+			if(*abort_error2)
+			{
+				delete_digit_read_iterator(dri1_p, transaction_id1, abort_error1);
+				delete_digit_read_iterator(dri2_p, transaction_id2, abort_error2);
+				return 0;
+			}
+		}
+
+		// if one or both of them does not have any more digits
+		if(data_size1 == 0 && data_size2 == 0) // both of them are prefixes of each other and are equal
+		{
+			(*is_prefix) = 1 | 2;
+			cmp = 0;
+			break;
+		}
+		// else shorter one is the prefix of the another
+		else if(data_size1 == 0 && data_size2 > 0)
+		{
+			(*is_prefix) = 1;
+			cmp = -1;
+			break;
+		}
+		else if(data_size1 > 0 && data_size2 == 0)
+		{
+			(*is_prefix) = 2;
+			cmp = 1;
+			break;
+		}
+
+		const uint32_t digits_processed = min(data_size1, data_size2);
+		for(uint32_t i = 0; i < digits_processed && cmp == 0; i++)
+			cmp = compare_numbers(data1[i], data2[i]);
+
+		data1 += digits_processed;
+		data2 += digits_processed;
+		data_size1 -= digits_processed;
+		data_size2 -= digits_processed;
+	}
+
+	delete_digit_read_iterator(dri1_p, transaction_id1, abort_error1);
+	delete_digit_read_iterator(dri2_p, transaction_id2, abort_error2);
 
 	return cmp * digits_requirement;
 }
