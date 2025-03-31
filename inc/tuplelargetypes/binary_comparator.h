781680e0d905966b0e6bb35cb6a548f5d4b0b1ac
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-31 21:06:20 +0530

    comparator now uses the new reader interface api

--- inc/tuplelargetypes/binary_comparator.h
+++ inc/tuplelargetypes/binary_comparator.h
@@ -1,18 +1,18 @@
 #ifndef BINARY_COMPARATOR_H
 #define BINARY_COMPARATOR_H
 
+#include<tuplelargetypes/binary_reader_interface.h>
+
+int compare_tb(binary_reader_interface* bri1_p, binary_reader_interface* bri2_p, int* is_prefix);
+
+#define compare_text compare_tb
+#define compare_blob compare_tb
+
 #include<tuplestore/tuple_def.h>
 #include<tuplestore/tuple.h>
 
 #include<tupleindexer/worm/worm.h>
 #include<tupleindexer/interface/page_access_methods.h>
-
-int compare_tb(const tuple_def* tpl_d1, const void* tupl1, positional_accessor inline_accessor1, const worm_tuple_defs* wtd1_p, const page_access_methods* pam1_p, const void* transaction_id1, int* abort_error1,
-				const tuple_def* tpl_d2, const void* tupl2, positional_accessor inline_accessor2, const worm_tuple_defs* wtd2_p, const page_access_methods* pam2_p, const void* transaction_id2, int* abort_error2,
-				int* is_prefix);
-
-#define compare_text compare_tb
-#define compare_blob compare_tb
 
 int compare_numeric(const tuple_def* tpl_d1, const void* tupl1, positional_accessor inline_accessor1, const worm_tuple_defs* wtd1_p, const page_access_methods* pam1_p, const void* transaction_id1, int* abort_error1,
 					const tuple_def* tpl_d2, const void* tupl2, positional_accessor inline_accessor2, const worm_tuple_defs* wtd2_p, const page_access_methods* pam2_p, const void* transaction_id2, int* abort_error2,
--- src/binary_comparator.c
+++ src/binary_comparator.c
@@ -1,33 +1,17 @@
 #include<tuplelargetypes/binary_comparator.h>
 
-#include<tuplelargetypes/common_extended.h>
-#include<tuplelargetypes/relative_positional_accessor.h>
-
-#include<tuplelargetypes/binary_read_iterator.h>
-
 #define BUFFER_CAPACITY 1024
 
-int compare_tb(const tuple_def* tpl_d1, const void* tupl1, positional_accessor inline_accessor1, const worm_tuple_defs* wtd1_p, const page_access_methods* pam1_p, const void* transaction_id1, int* abort_error1,
-				const tuple_def* tpl_d2, const void* tupl2, positional_accessor inline_accessor2, const worm_tuple_defs* wtd2_p, const page_access_methods* pam2_p, const void* transaction_id2, int* abort_error2,
-				int* is_prefix)
+int compare_tb(binary_reader_interface* bri1_p, binary_reader_interface* bri2_p, int* is_prefix)
 {
 	(*is_prefix) = 0;
 
-	int is_null1;
-	{
-		user_value uval1;
-		if(!get_value_from_element_from_tuple(&uval1, tpl_d1, inline_accessor1, tupl1))
-			return -2;
-		is_null1 = is_user_value_NULL(&uval1);
-	}
+	// if any one if it is not valid return -2
+	if((!bri1_p->is_valid(bri1_p)) || (!bri2_p->is_valid(bri2_p)))
+		return -2;
 
-	int is_null2;
-	{
-		user_value uval2;
-		if(!get_value_from_element_from_tuple(&uval2, tpl_d2, inline_accessor2, tupl2))
-			return -2;
-		is_null2 = is_user_value_NULL(&uval2);
-	}
+	int is_null1 = bri1_p->is_null(bri1_p);
+	int is_null2 = bri2_p->is_null(bri2_p);
 
 	// if one of them is NULL
 	if(is_null1 && is_null2)
@@ -45,32 +29,31 @@ int compare_tb(const tuple_def* tpl_d1, const void* tupl1, positional_accessor i
 	char* data1 = buffer1; uint32_t data_size1 = 0;
 	char* data2 = buffer2; uint32_t data_size2 = 0;
 
-	binary_read_iterator* bri1_p = get_new_binary_read_iterator(tupl1, tpl_d1, inline_accessor1, wtd1_p, pam1_p);
-	binary_read_iterator* bri2_p = get_new_binary_read_iterator(tupl2, tpl_d2, inline_accessor2, wtd2_p, pam2_p);
-
 	int cmp = 0;
 	while(cmp == 0)
 	{
 		if(data_size1 == 0) // if there are no more bytes in the data1 then read it
 		{
+			int error = 0;
 			data1 = buffer1;
-			data_size1 = read_from_binary_read_iterator(bri1_p, data1, BUFFER_CAPACITY, transaction_id1, abort_error1);
-			if(*abort_error1)
+			data_size1 = bri1_p->read_bytes_as_stream(bri1_p, data1, BUFFER_CAPACITY, &error);
+			if(error)
 			{
-				delete_binary_read_iterator(bri1_p, transaction_id1, abort_error1);
-				delete_binary_read_iterator(bri2_p, transaction_id2, abort_error2);
+				bri1_p->close_bytes_stream(bri1_p);
+				bri2_p->close_bytes_stream(bri2_p);
 				return 0;
 			}
 		}
 
 		if(data_size2 == 0) // if there are no more bytes in the data2 then read it
 		{
+			int error = 0;
 			data2 = buffer2;
-			data_size2 = read_from_binary_read_iterator(bri2_p, data2, BUFFER_CAPACITY, transaction_id2, abort_error2);
-			if(*abort_error2)
+			data_size2 = bri2_p->read_bytes_as_stream(bri2_p, data2, BUFFER_CAPACITY, &error);
+			if(error)
 			{
-				delete_binary_read_iterator(bri1_p, transaction_id1, abort_error1);
-				delete_binary_read_iterator(bri2_p, transaction_id2, abort_error2);
+				bri1_p->close_bytes_stream(bri1_p);
+				bri2_p->close_bytes_stream(bri2_p);
 				return 0;
 			}
 		}
@@ -105,10 +88,15 @@ int compare_tb(const tuple_def* tpl_d1, const void* tupl1, positional_accessor i
 		data_size2 -= bytes_processed;
 	}
 
-	delete_binary_read_iterator(bri1_p, transaction_id1, abort_error1);
-	delete_binary_read_iterator(bri2_p, transaction_id2, abort_error2);
+	bri1_p->close_bytes_stream(bri1_p);
+	bri2_p->close_bytes_stream(bri2_p);
 	return cmp;
 }
+
+#include<tuplelargetypes/common_extended.h>
+#include<tuplelargetypes/relative_positional_accessor.h>
+
+#include<tuplelargetypes/binary_read_iterator.h>
 
 #include<tuplelargetypes/numeric_extended.h>
 #include<tuplelargetypes/digit_read_iterator.h>
