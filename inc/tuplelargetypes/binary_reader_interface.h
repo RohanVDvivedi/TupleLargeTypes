f6a80b28db4a9902949c17fc37a33a34d2cf93de
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-31 20:53:37 +0530

    added reader interface to be built using the tuple itself

--- inc/tuplelargetypes/binary_reader_interface.h
+++ inc/tuplelargetypes/binary_reader_interface.h
@@ -1,10 +1,13 @@
 #ifndef BINARY_READER_INTERFACE_H
 #define BINARY_READER_INTERFACE_H
 
+#include<tuplestore/tuple.h>
+#include<tuplestore/tuple_def.h>
+
 typedef struct binary_reader_interface binary_reader_interface;
 struct binary_reader_interface
 {
-	const void* context;
+	void* context;
 
 	int (*is_valid)(binary_reader_interface* bri_p);
 	// call below functions only if the above one returns 1
@@ -12,7 +15,51 @@ struct binary_reader_interface
 	int (*is_null)(binary_reader_interface* bri_p);
 	// call below functions only if the above one returns 1
 
-	uint32_t (*read_bytes_as_stream)(binary_read_iterator* bri_p, char* data, uint32_t data_size, int* error);
+	uint32_t (*read_bytes_as_stream)(binary_reader_interface* bri_p, char* data, uint32_t data_size, int* error);
+
+	// to be called on an error OR EOF OR when you do not need to read any more data
+	void (*close_bytes_stream)(binary_reader_interface* bri_p);
 };
+
+#define text_reader_interface binary_reader_interface
+#define blob_reader_interface binary_reader_interface
+
+/*
+	implementation for a text/blob inside a tuple (extended or inline)
+*/
+
+#include<tuplelargetypes/binary_read_iterator.h>
+
+typedef struct intuple_binary_reader_interface_context intuple_binary_reader_interface_context;
+struct intuple_binary_reader_interface_context
+{
+	const tuple_def* tpl_d;
+	const void* tupl;
+	positional_accessor inline_accessor;
+	const worm_tuple_defs* wtd_p;
+	const page_access_methods* pam_p;
+	const void* transaction_id;
+	int* abort_error;
+
+	binary_read_iterator* bri_p;
+};
+
+int is_valid_for_intuple_binary_reader_interface(binary_reader_interface* bri_p);
+int is_null_for_intuple_binary_reader_interface(binary_reader_interface* bri_p);
+uint32_t read_bytes_as_stream_for_intuple_binary_reader_interface(binary_reader_interface* bri_p, char* data, uint32_t data_size, int* error);
+void close_bytes_stream_for_intuple_binary_reader_interface(binary_reader_interface* bri_p);
+
+#define init_intuple_binary_reader_interface(tpl_d_v, tupl_v, inline_accessor_v, wtd_p_v, pam_p_v, transaction_id_v, abort_error_v) \
+	(binary_reader_interface){&(intuple_binary_reader_interface_context){tpl_d_v, tupl_v, inline_accessor_v, wtd_p_v, pam_p_v, transaction_id_v, abort_error_v, NULL}, \
+	is_valid_for_intuple_binary_reader_interface, \
+	is_null_for_intuple_binary_reader_interface, \
+	read_bytes_as_stream_for_intuple_binary_reader_interface, \
+	close_bytes_stream_for_intuple_binary_reader_interface, \
+};
+
+
+/*
+	implementation for a text/blob inside a user_value (extended or inline)
+*/
 
 #endif
---
+++ src/binary_reader_interface.c
@@ -0,0 +1,62 @@
+#include<tuplelargetypes/binary_reader_interface.h>
+
+/*
+	implementation for a text/blob inside a tuple (extended or inline)
+*/
+
+int is_valid_for_intuple_binary_reader_interface(binary_reader_interface* bri_p)
+{
+	intuple_binary_reader_interface_context* cntxt = bri_p->context;
+
+	user_value uval;
+	int is_valid = get_value_from_element_from_tuple(&uval, cntxt->tpl_d, cntxt->inline_accessor, cntxt->tupl);
+
+	return is_valid;
+}
+
+int is_null_for_intuple_binary_reader_interface(binary_reader_interface* bri_p)
+{
+	intuple_binary_reader_interface_context* cntxt = bri_p->context;
+
+	user_value uval;
+	int is_valid = get_value_from_element_from_tuple(&uval, cntxt->tpl_d, cntxt->inline_accessor, cntxt->tupl);
+	if(!is_valid) // if not valid, then this value is close to being NULL
+		return 1;
+
+	return is_user_value_NULL(&uval);
+}
+
+uint32_t read_bytes_as_stream_for_intuple_binary_reader_interface(binary_reader_interface* bri_p, char* data, uint32_t data_size, int* error)
+{
+	intuple_binary_reader_interface_context* cntxt = bri_p->context;
+
+	if((*cntxt->abort_error))
+	{
+		(*error) = (*cntxt->abort_error);
+		return 0;
+	}
+
+	if(cntxt->bri_p == NULL)
+		cntxt->bri_p = get_new_binary_read_iterator(cntxt->tupl, cntxt->tpl_d, cntxt->inline_accessor, cntxt->wtd_p, cntxt->pam_p);
+
+	data_size = read_from_binary_read_iterator(cntxt->bri_p, data, data_size, cntxt->transaction_id, cntxt->abort_error);
+	(*error) = (*cntxt->abort_error);
+	if((*cntxt->abort_error))
+	{
+		delete_binary_read_iterator(cntxt->bri_p, cntxt->transaction_id, cntxt->abort_error);
+		cntxt->bri_p = NULL;
+		return 0;
+	}
+
+	return data_size;
+}
+
+void close_bytes_stream_for_intuple_binary_reader_interface(binary_reader_interface* bri_p)
+{
+	intuple_binary_reader_interface_context* cntxt = bri_p->context;
+	delete_binary_read_iterator(cntxt->bri_p, cntxt->transaction_id, cntxt->abort_error);
+}
+
+/*
+	implementation for a text/blob inside a user_value (extended or inline)
+*/
