3477b569c6a4a08c16068d4edebb14457ed87637
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-01 10:28:57 +0530

    numeric reader interface implemented

--- src/numeric_reader_interface.c
+++ src/numeric_reader_interface.c
@@ -43,7 +43,27 @@ void extract_sign_bits_and_exponent_for_intuple_numeric_reader_interface(numeric
 
 uint32_t read_digits_as_stream_for_intuple_numeric_reader_interface(numeric_reader_interface* nri_p, uint64_t* digits, uint32_t digits_size, int* error)
 {
+	intuple_numeric_reader_interface_context* cntxt = nri_p->context;
 
+	if((*(cntxt->abort_error)))
+	{
+		(*error) = (*cntxt->abort_error);
+		return 0;
+	}
+
+	if(cntxt->dri_p == NULL)
+		cntxt->dri_p = get_new_digit_read_iterator(cntxt->tupl, cntxt->tpl_d, cntxt->inline_accessor, cntxt->wtd_p, cntxt->pam_p);
+
+	digits_size = read_from_digit_read_iterator(cntxt->dri_p, digits, digits_size, cntxt->transaction_id, cntxt->abort_error);
+	(*error) = (*cntxt->abort_error);
+	if((*(cntxt->abort_error)))
+	{
+		delete_digit_read_iterator(cntxt->dri_p, cntxt->transaction_id, cntxt->abort_error);
+		cntxt->dri_p = NULL;
+		return 0;
+	}
+
+	return digits_size;
 }
 
 void close_digits_stream_for_intuple_numeric_reader_interface(numeric_reader_interface* nri_p)
