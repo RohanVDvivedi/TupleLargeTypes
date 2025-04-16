d2d6c89e6d02265c18378360ac936f8dc73c04ac
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-16 17:08:18 +0530

    added interface to write jsonb to binary write iterator

--- inc/tuplelargetypes/jsonb_writer_interface.h
+++ inc/tuplelargetypes/jsonb_writer_interface.h
@@ -8,4 +8,24 @@ struct jsonb_writer_interface
 	void (*write_jsonb_bytes)(void* context_p, const char* bytes, uint32_t bytes_size, int* error);
 };
 
+/*
+	implementation for writing to a binary_write_iterator
+*/
+
+typedef struct jsonb_to_binary_write_iterator_context jsonb_to_binary_write_iterator_context;
+struct jsonb_to_binary_write_iterator_context
+{
+	binary_write_iterator* bwi_p;
+	const void* transaction_id;
+	int* abort_error;
+};
+
+void jsonb_to_binary_write_iterator_write_jsonb_bytes(void* context_p, const char* bytes, uint32_t bytes_size, int* error);
+
+#define init_jsonb_to_binary_write_iterator_interface(bwi_p_v, transaction_id_v, abort_error_v) \
+(binary_reader_interface){ \
+	&(jsonb_to_binary_write_iterator_context){bwi_p_v, transaction_id_v, abort_error_v}, \
+	jsonb_to_binary_write_iterator_write_jsonb_bytes, \
+};
+
 #endif
