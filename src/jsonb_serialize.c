532de0608037481f69b5356013528906ffc4ac9c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-16 20:16:48 +0530

    added utility function to write uint8_t and uint32_t to jsonb writer in a single call

--- src/jsonb_serialize.c
+++ src/jsonb_serialize.c
@@ -1,5 +1,20 @@
 #include<tuplelargetypes/jsonb_serialize.h>
 
+#include<serint/serial_int.h>
+
+static inline void jsonb_writer_interface_write_uint8(jsonb_writer_interface* jwi_p, uint8_t b, int* error)
+{
+	char byte = b;
+	jwi_p->write_jsonb_bytes(jwi_p, &byte, 1, error);
+}
+
+static inline void jsonb_writer_interface_write_uint32(jsonb_writer_interface* jwi_p, uint32_t b, int* error)
+{
+	char bytes[4];
+	serialize_uint32(bytes, 4, b);
+	jwi_p->write_jsonb_bytes(jwi_p, bytes, 4, error);
+}
+
 int jsonb_serialize(const jsonb_writer_interface* jwi_p, jsonb_node* node_p)
 {
 	// TODO
