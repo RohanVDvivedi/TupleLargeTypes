58571bd912df75df2c41cd7be935185991f04b4e
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-16 18:11:59 +0530

    minor api change

--- inc/tuplelargetypes/jsonb_serialize.h
+++ inc/tuplelargetypes/jsonb_serialize.h
@@ -4,6 +4,7 @@
 #include<tuplelargetypes/jsonb_writer_interface.h>
 
 // only a successfully finalized jsonb_node can be serialized
-void jsonb_serialize(const jsonb_writer_interface* jwi_p, jsonb_node* node_p);
+// returns 1 for success, OR 0 in an error, consult your jsonb_writer_interface upon an error
+int jsonb_serialize(const jsonb_writer_interface* jwi_p, jsonb_node* node_p);
 
 #endif
