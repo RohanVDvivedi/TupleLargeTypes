0b5304cdb68e8fabc575fc20983387745aeccb6e
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-16 18:15:27 +0530

    added necessary headers for the declaration

--- inc/tuplelargetypes/jsonb_serialize.h
+++ inc/tuplelargetypes/jsonb_serialize.h
@@ -2,8 +2,9 @@
 #define JSONB_SERIALIZE_H
 
 #include<tuplelargetypes/jsonb_writer_interface.h>
+#include<tuplelargetypes/jsonb_node.h>
 
-// only a successfully finalized jsonb_node can be serialized
+// only a successfully finalized jsonb_node must be serialized, else it will result in undefined behaviour
 // returns 1 for success, OR 0 in an error, consult your jsonb_writer_interface upon an error
 int jsonb_serialize(const jsonb_writer_interface* jwi_p, jsonb_node* node_p);
 
