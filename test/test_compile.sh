ad2cd89c3e2bfe8960af9a913e7038f71f73a322
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-19 17:32:37 +0530

    started adding jsonb test cases

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -3,3 +3,5 @@ gcc ./test_numeric.c -o test_n.out -ltuplelargetypes -ltupleindexer -lrwlock -lt
 gcc ./test_tuple_list.c -o test_tl.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
 
 gcc ./test_materialized_numeric.c -o test_mn.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
+
+gcc ./test_jsonb.c -o test_jb.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
