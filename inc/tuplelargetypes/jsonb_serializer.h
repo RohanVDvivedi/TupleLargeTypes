3ca4c74c15951d950d8cf2335f209c349cd7928b
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-19 13:36:04 +0530

    minor comment fix

--- inc/tuplelargetypes/jsonb_serializer.h
+++ inc/tuplelargetypes/jsonb_serializer.h
@@ -5,7 +5,7 @@
 #include<tuplelargetypes/jsonb_node.h>
 
 // only a successfully finalized jsonb_node must be serialized, else it will result in undefined behaviour
-// returns 1 for success, OR 0 in an error, consult your jsonb_writer_interface upon an error
+// returns 1 for success, OR 0 in an error, consult your stream upon an error (when 0 is returned)
 int serialize_jsonb(stream* ws, const jsonb_node* node_p);
 
 #endif
