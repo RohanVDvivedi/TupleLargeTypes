f2c6316dbf855febe438d6fa8a1da188fcfc424d
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-19 17:39:55 +0530

    jsonb headers formalized and appropriate public ap headers added

--- inc/tuplelargetypes/jsonb_extended.h
+++ inc/tuplelargetypes/jsonb_extended.h
@@ -12,9 +12,9 @@ data_type_info* get_jsonb_extended_type_info(uint32_t max_size, uint32_t inline_
 
 #include<tuplelargetypes/common_extended.h>
 
-#include<jsonb_node.h>
-#include<jsonb_parser.h>
-#include<jsonb_serializer.h>
+#include<tuplelargetypes/jsonb_node.h>
+#include<tuplelargetypes/jsonb_parser.h>
+#include<tuplelargetypes/jsonb_serializer.h>
 
 #include<tuplelargetypes/binary_read_iterator.h>
 #include<tuplelargetypes/binary_write_iterator.h>
