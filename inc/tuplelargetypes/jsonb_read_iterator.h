b4e573518ea3bd19019b2bf455018edb4a15072a
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-30 00:20:48 +0530

    jsonb_read_iterator declarations added

--- inc/tuplelargetypes/jsonb_read_iterator.h
+++ inc/tuplelargetypes/jsonb_read_iterator.h
@@ -1,7 +1,11 @@
 #ifndef JSONB_READ_ITERATOR_H
 #define JSONB_READ_ITERATOR_H
 
+#include<stdint.h>
+#include<cutlery/dstring.h>
+
 // similar to json_key
+typedef struct jsonb_key jsonb_key;
 struct jsonb_key
 {
 	int is_array_index; // if this key indexes into an element
@@ -13,19 +17,23 @@ struct jsonb_key
 	dstring key; // valid only if !(not) is_array_index
 };
 
+typedef struct jsonb_accessor jsonb_accessor;
 struct jsonb_accessor
 {
 	uint32_t keys_capacity; // number of available elements at keys_list
 
 	uint32_t keys_count; // number of elements at keys_list
 
-	jsonb_keys* keys_list;
+	jsonb_key* keys_list;
 };
 
 #define EMPTY_JSONB_ACCESSOR (jsonb_accessor){.keys_capacity = 0, .keys_count = 0, .keys_list = NULL}
 
 // the above two structures just assist in completing the valued curr_acs attribute below
 
+#include<tuplelargetypes/binary_read_iterator.h>
+
+typedef struct jsonb_read_iterator jsonb_read_iterator;
 struct jsonb_read_iterator
 {
 	jsonb_accessor curr_acs; // contains dynamically allocated keys for the current position
@@ -34,6 +42,8 @@ struct jsonb_read_iterator
 };
 
 #define init_jsonb_read_iterator(bri_p_v) (jsonb_read_iterator){.curr_acs = EMPTY_JSONB_ACCESSOR, .bri_p = bri_p_v}
+
+#include<jsonparser/json_accessor.h>
 
 // returns 1 only if the json_read_iterator now exactly points to the acs accessor passed
 int point_to_accessor_for_jsonb_read_iterator(jsonb_read_iterator* jri_p, const json_accessor* acs, const void* transaction_id, int* abort_error);
---
+++ src/jsonb_read_iterator.c
@@ -0,0 +1,6 @@
+#include<tuplelargetypes/jsonb_read_iterator.h>
+
+static int compare_jsonb_N_json_accessors(const jsonb_accessor* jb_acs, const json_accessor* j_acs, int* is_prefix)
+{
+	// TODO
+}
