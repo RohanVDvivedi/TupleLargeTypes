f5a3743a5fd220988456e3983d57818c48a25d47
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-30 00:33:04 +0530

    added declarations to be implemented for jsonb_reade_iterator

--- src/jsonb_read_iterator.c
+++ src/jsonb_read_iterator.c
@@ -1,6 +1,32 @@
 #include<tuplelargetypes/jsonb_read_iterator.h>
 
+// the ownership of key is transferred to the jb_acs
+static inline int push_to_jsonb_accessor(jsonb_accessor* jb_acs, int is_array_index, uint32_t index, uint32_t total_siblings_count, dstring key_consumed)
+{
+	// TODO
+}
+
+static inline int pop_from_jsonb_accessor(jsonb_accessor* jb_acs)
+{
+	// TODO
+}
+
+static inline int overwrite_top_key_in_jsonb_accessor(jsonb_accessor* jb_acs, dstring key_consumed)
+{
+	// TODO
+}
+
 static int compare_jsonb_N_json_accessors(const jsonb_accessor* jb_acs, const json_accessor* j_acs, int* is_prefix)
+{
+	// TODO
+}
+
+int point_to_accessor_for_jsonb_read_iterator(jsonb_read_iterator* jri_p, const json_accessor* acs, const void* transaction_id, int* abort_error)
+{
+	// TODO
+}
+
+binary_read_iterator* get_cloned_iterator_for_jsonb_read_iterator(const jsonb_read_iterator* jri_p, const json_accessor* acs, const void* transaction_id, int* abort_error)
 {
 	// TODO
 }
