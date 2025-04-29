0c60c214a940117802ef2df6de77e583e77320b3
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-29 16:11:29 +0530

    numeric extended format explaining comments fixed

--- inc/tuplelargetypes/numeric_extended.h
+++ inc/tuplelargetypes/numeric_extended.h
@@ -64,17 +64,17 @@ int compare_numeric_prefix_no_digits(numeric_sign_bits s1, int16_t e1, numeric_s
 
 /*
 	INLINE NUMERIC TYPE
-	numeric type here is represented as a base 10^12 number, with each digit being a 5 byte unsigned number between [0, 10^12), in-memory representation of each digit will be a uint64_t
+	numeric type here is represented as a base 10^12 number, with each digit being a 5 byte unsigned number between [0, 10^12) and in-memory representation of each digit will be a uint64_t
 	each inline numeric type is composed of
-		* 3 sign bits ==> 0 -> -infinity, 1 -> negative number, 2 -> zero (there are not +/- zeros just a zero), 3 -> positive number and 4 -> +infinity
+		* 3 sign bits ==> 0 -> -infinity, 1 -> negative number, 2 -> zero (there are no +/- zeros, just one zero), 3 -> positive number and 4 -> +infinity
 		* 2 byte signed exponent
 		* some N number of digits as an array of 5 byte unsigned integers
 
-	Explanation of choosing a 5 byte integer to store 12 decimal digits of information comes from the following
-		* storing mantissa is a big problem, we need to store digit groups as unsigned integers
+	Explanation for choosing a 5 byte integer to store 12 decimal digits of information comes from the following logic
+		* storing mantissa is a big problem, we need to store group of decimal digits as unsigned integers
 		* an X byte big unsigned integer can store Y digits, then Y = log(256 ^ X) / log(10) = X * 8 * log(2) / log(10)
 		* the table goes as follows
-			X |    Y   ->  ceil(Y) |  Y/X
+			X |    Y   ->  ceil(Y) |  ceil(Y)/X
 			1 |  2.408 ->  2       |  2
 			2 |  4.816 ->  4       |  2
 			3 |  7.224 ->  7       |  2.333
@@ -84,11 +84,10 @@ int compare_numeric_prefix_no_digits(numeric_sign_bits s1, int16_t e1, numeric_s
 			7 | 16.857 -> 16       |  2.285
 			8 | 19.265 -> 19       |  2.375
 		* if you analyze carefully the most value of digits per bytes is the most efficient way to go, which is using 5 byte unsigned integer to store a group of 12 decimal digits
-		* first task is to build a function to set one of the 12 digits in a uint64_t
 		* now since we are storing digits in base 10^12 in a 5 byte integer, why not store exponent represented as a (10^12) ^ exponent
 		* now everything becomes radix 10^12 instead of radix of 10, and with 3 sign bits, 16 bit exponent and finally an array of 5 byte integers, each storing precisely 12 digits worth of information as a 10^12 radix digit
 
 	EXTENDED NUMERIC TYPE
 	will just be a tuple of a inline numeric type and a page_id to point to a worm
-	any and all following digit that could not be accomodated in the prefix will be pushed into the worm in 5 byte chunks one after the another
+	any and all following digits that could not be accomodated in the prefix will be pushed into the worm in 5 byte chunks one after the another
 */
