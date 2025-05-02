5612e74458d32cae84bc5a4b782f45d664a066a0
RohanVDvivedi <rohandvivedi@gmail.com>
2025-05-03 01:46:03 +0530

    added more test cases for jsonb point to

--- test/test_jsonb.c
+++ test/test_jsonb.c
@@ -379,6 +379,15 @@ int main()
 	deinit_jsonb_read_iterator(&jri);
 	delete_binary_read_iterator(bri_p, transaction_id, &abort_error);
 
+	bri_p = get_new_binary_read_iterator(inline_tuple, tpl_d, ACCS, &wtd, pam_p);
+	jri = init_jsonb_read_iterator(bri_p);
+
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("aAa_revenues"), JSON_ARRAY_INDEX(1)));
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("projects"), JSON_ARRAY_INDEX(2)));
+
+	deinit_jsonb_read_iterator(&jri);
+	delete_binary_read_iterator(bri_p, transaction_id, &abort_error);
+
 	/* TESTS ENDED */
 
 	/* CLEANUP */
