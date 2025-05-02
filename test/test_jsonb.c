f552d8d25000c36af1bee57a52073b240a5c42cf
RohanVDvivedi <rohandvivedi@gmail.com>
2025-05-03 01:00:15 +0530

    added logic to test json_read_iterator

--- test/test_jsonb.c
+++ test/test_jsonb.c
@@ -353,6 +353,20 @@ int main()
 	deinit_jsonb_read_iterator(&jri);
 	delete_binary_read_iterator(bri_p, transaction_id, &abort_error);
 
+	bri_p = get_new_binary_read_iterator(inline_tuple, tpl_d, ACCS, &wtd, pam_p);
+	jri = init_jsonb_read_iterator(bri_p);
+
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR());
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("projects")));
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("projects"), JSON_ARRAY_INDEX(2)));
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("projects"), JSON_ARRAY_INDEX(12)));
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("projects"), JSON_ARRAY_INDEX(3)));
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("votes-right")));
+	jsonb_read_iterator_parse_for_accessor(&jri, STATIC_JSON_ACCESSOR(JSON_OBJECT_KEY_literal("age")));
+
+	deinit_jsonb_read_iterator(&jri);
+	delete_binary_read_iterator(bri_p, transaction_id, &abort_error);
+
 	/* TESTS ENDED */
 
 	/* CLEANUP */
