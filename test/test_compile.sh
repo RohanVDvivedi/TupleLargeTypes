5bee92b22e78bb2a2f8c5cf804cca1421bf19e2a
RohanVDvivedi <rohandvivedi@gmail.com>
2025-05-19 22:55:56 +0530

    rwlock -> lockking

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,7 +1,7 @@
-gcc ./test_text_blob.c -o test_tb.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
-gcc ./test_numeric.c -o test_n.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
-gcc ./test_tuple_list.c -o test_tl.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
+gcc ./test_text_blob.c -o test_tb.out -ltuplelargetypes -ltupleindexer -llockking -ltuplestore -lcutlery
+gcc ./test_numeric.c -o test_n.out -ltuplelargetypes -ltupleindexer -llockking -ltuplestore -lcutlery
+gcc ./test_tuple_list.c -o test_tl.out -ltuplelargetypes -ltupleindexer -llockking -ltuplestore -lcutlery
 
-gcc ./test_materialized_numeric.c -o test_mn.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
+gcc ./test_materialized_numeric.c -o test_mn.out -ltuplelargetypes -ltupleindexer -llockking -ltuplestore -lcutlery
 
-gcc ./test_jsonb.c -o test_jb.out -ltuplelargetypes -ltupleindexer -lrwlock -ltuplestore -lcutlery
+gcc ./test_jsonb.c -o test_jb.out -ltuplelargetypes -ltupleindexer -llockking -ltuplestore -lcutlery
