d3441b456b9be2d1c2c4319b153f40a28b362d91
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-18 01:02:08 +0530

    minor comment fix and added new tasks

--- inc/tuplelargetypes/comparator.h
+++ inc/tuplelargetypes/comparator.h
@@ -15,7 +15,7 @@ int compare_numeric(const numeric_reader_interface* nri1_p, const numeric_reader
 /*
 	if first parameter is prefix of another then (is_prefix & 1) returns true
 	if second parameter is prefix of another then (is_prefix & 2) returns true
-	if both are equal strings or binarys, the both the above conditions return true
+	if both are equal strings or binarys or numerics, the both the above conditions return true
 	is_prefix will only be set if both the parameters are not NULL
 */
 
