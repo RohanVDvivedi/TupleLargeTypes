44bb936de209b18b17a3eaec9503787933ec1c5d
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-19 21:29:29 +0530

    --

--- inc/tuplelargetypes/digit_write_iterator.h
+++ inc/tuplelargetypes/digit_write_iterator.h
@@ -16,18 +16,18 @@
 typedef struct digit_write_iterator digit_write_iterator;
 struct digit_write_iterator
 {
-	int is_inline:1;
+	int is_extended:1;
 
-	// shallow copy attributes
+	// shallow copy of input parameters
 	void* tupl;
 	const tuple_def* tpl_d;
-	positional_accessor inline_accessor;
+	positional_accessor pos;
 
-	uint32_t digits_to_be_written_to_prefix;
 	uint32_t digits_written_to_prefix;
+	uint32_t digits_to_be_written_to_prefix;
 	// no more appending to prefix if the above 2 attributes become equal
 
-	// unused if is_inline is set
+	// used only if is_extended is set
 	worm_append_iterator* wai_p;
 
 	// below attributes only to be used to initialize the wai, only upon requirement
@@ -36,13 +36,8 @@ struct digit_write_iterator
 	const page_modification_methods* pmm_p;
 };
 
-/*
-	prefix is appended to a non-persistent tuple, making it as much larger as possible (also controller using digits_to_be_written_to_prefix parameter)
-	suffix is transaction protected, and could be persistent
-*/
-
 // the numeric in context must be EMPTY_DATUM or partially written
-digit_write_iterator* get_new_digit_write_iterator(void* tupl, const tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t digits_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p);
+digit_write_iterator* get_new_digit_write_iterator(void* tupl, const tuple_def* tpl_d, positional_accessor pos, uint32_t digits_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p);
 
 void delete_digit_write_iterator(digit_write_iterator* dwi_p, const void* transaction_id, int* abort_error);
 
