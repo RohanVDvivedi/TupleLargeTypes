14b662253eca8546435b3ab57d755b3073de7289
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-19 12:27:40 +0530

    new simplified implementation for the peek for the binary read iterator

--- src/binary_read_iterator.c
+++ src/binary_read_iterator.c
@@ -169,77 +169,29 @@ uint32_t read_from_binary_read_iterator(binary_read_iterator* bri_p, char* data,
 
 const char* peek_in_binary_read_iterator(binary_read_iterator* bri_p, uint32_t* data_size, const void* transaction_id, int* abort_error)
 {
-	// cache the prefix and the extension_head_page_id in local variables
-	datum prefix = (*EMPTY_DATUM);
-	uint64_t extension_head_page_id = bri_p->pam_p->pas.NULL_PAGE_ID;
+	if(bri_p->is_null)
+		return NULL;
+
+	if(is_empty_dstring(&(bri_p->curr_chunk)))
 	{
-		relative_positional_accessor child_relative_accessor;
-		initialize_relative_positional_accessor(&child_relative_accessor, &(bri_p->inline_accessor), 1 * (!(bri_p->is_inline)));
+		if(bri_p->extension_head_page_id == bri_p->pam_p->pas.NULL_PAGE_ID)
+			break;
 
-		if(bri_p->is_inline)
-		{
-			point_to_prefix(&child_relative_accessor, bri_p->is_inline);
-			int valid_prefix = get_value_from_element_from_tuple(&prefix, bri_p->tpl_d, child_relative_accessor.exact, bri_p->tupl);
-			if((!valid_prefix) || is_datum_NULL(&prefix)) // this means it is an uninitialized large text or binary, so treat it as if it is empty, with no worm following it
-				prefix = (*EMPTY_DATUM);
-		}
-		else
-		{
-			point_to_prefix(&child_relative_accessor, bri_p->is_inline);
-			int valid_prefix = get_value_from_element_from_tuple(&prefix, bri_p->tpl_d, child_relative_accessor.exact, bri_p->tupl);
-			if((!valid_prefix) || is_datum_NULL(&prefix)) // this means it is an uninitialized large text or binary, so treat it as if it is empty, with no worm following it
-			{
-				prefix = (*EMPTY_DATUM);
-				extension_head_page_id = bri_p->pam_p->pas.NULL_PAGE_ID;
-			}
-			else // else you need to read the extension_head_page_id
-			{
-				if(bri_p->wri_p == NULL) // you will need extension_head_page_id set to valid value only if the worm_read_iterator is not initialized
-				{
-					datum worm_head_page_id;
-					point_to_extension_head_page_id(&child_relative_accessor, bri_p->is_inline);
-					int valid_worm_head_page_id = get_value_from_element_from_tuple(&worm_head_page_id, bri_p->tpl_d, child_relative_accessor.exact, bri_p->tupl);
-					if(valid_worm_head_page_id && !is_datum_NULL(&worm_head_page_id)) // if valid and not NULL
-						extension_head_page_id = worm_head_page_id.uint_value;
-				}
-			}
-		}
-
-		deinitialize_relative_positional_accessor(&child_relative_accessor);
-	}
-
-	const char* data = NULL;
-	(*data_size) = 0;
-
-	if(bri_p->bytes_read_from_prefix < prefix.string_or_binary_size) // peek from prefix until it is not completely read
-	{
-		(*data_size) = (prefix.string_or_binary_size - bri_p->bytes_read_from_prefix);
-		data = prefix.string_or_binary_value + bri_p->bytes_read_from_prefix;
-	}
-	else if(!(bri_p->is_inline)) // go here only if it is a extended text or binary
-	{
-		// initialize worm read iterator if not done already
 		if(bri_p->wri_p == NULL)
 		{
-			if(extension_head_page_id == bri_p->pam_p->pas.NULL_PAGE_ID)
+			bri_p->wri_p = get_new_worm_read_iterator(bri_p->extension_head_page_id, bri_p->wtd_p, bri_p->pam_p, transaction_id, abort_error);
+			if(*abort_error)
 				return NULL;
-
-			bri_p->wri_p = get_new_worm_read_iterator(extension_head_page_id, bri_p->wtd_p, bri_p->pam_p, transaction_id, abort_error);
-			if(*abort_error) // on abort error, do nothing
-			{
-				bri_p->wri_p = NULL;
-				return NULL;
-			}
 		}
 
-		data = peek_in_worm(bri_p->wri_p, data_size, transaction_id, abort_error);
-		if(*abort_error) // on abort error, delete worm iterator and set it to NULL
-		{
-			delete_worm_read_iterator(bri_p->wri_p, transaction_id, abort_error);
-			bri_p->wri_p = NULL;
+		uint32_t data_size = 0;
+		void* data = peek_in_worm(bri_p->wri_p, &data_size, transaction_id, abort_error);
+		if(*abort_error)
 			return NULL;
-		}
+
+		bri_p->curr_chunk = get_dstring_pointing_to(data, data_size);
 	}
 
-	return data;
+	(*data_size) = min(get_char_count_dstring(&(bri_p->curr_chunk)), UINT32_MAX);
+	return get_byte_array_dstring(&(bri_p->curr_chunk));
 }
