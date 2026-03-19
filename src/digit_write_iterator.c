a6719bd350643a4b42b7f2bf4ea9da2ac8a4ade0
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-19 21:26:19 +0530

    removed header

--- src/digit_write_iterator.c
+++ src/digit_write_iterator.c
@@ -6,8 +6,6 @@
 
 #include<tuplelargetypes/relative_positional_accessor.h>
 
-#include<tuplelargetypes/digit_iterator_commons.h>
-
 #include<tuplelargetypes/numeric_extended.h>
 
 digit_write_iterator* get_new_digit_write_iterator(void* tupl, const tuple_def* tpl_d, positional_accessor inline_accessor, uint32_t digits_to_be_written_to_prefix, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p)
