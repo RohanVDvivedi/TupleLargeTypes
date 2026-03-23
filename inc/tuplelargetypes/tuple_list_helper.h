812d34ef03e06749ec2d893b49537eb0e3becff4
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-23 20:42:41 +0530

    new peekable api for tuple_list finalized

--- inc/tuplelargetypes/tuple_list_helper.h
+++ inc/tuplelargetypes/tuple_list_helper.h
@@ -3,10 +3,18 @@
 
 #include<tuplelargetypes/binary_read_iterator.h>
 
+// this function returns the size of the tuple that this iterator is pointing to
+uint32_t get_curr_tuple_size_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
+
 // returns 1 if a tuple was skipped, else returns 0 for abort_error OR end of data
 int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
 
-// returns a pointer to a next tuple read (free_the_pointer = 1) OR peeked (free_the_pointer = 0) from the iterator, else returns NULL for abort_error OR end of data
-void* get_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, int* free_the_pointer, const void* transaction_id, int* abort_error);
+// first try peeking, if the tuple you are pointing to fits in the chunk itself, then a pointer to the tuple in the chunk is returned
+// you do not need to free this pointer but you will need to skip this tuple after use of this pointer
+const void* peek_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
+
+// then try reading it, this function does a force memcpy of the tuple from the iterator into a newlu malloc-ed buffer
+// you will need to free this buffer, the tuple returned by this function is already skipped
+void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
 
 #endif
--- src/tuple_list_helper.c
+++ src/tuple_list_helper.c
@@ -6,83 +6,114 @@ typedef struct get_size_context get_size_context;
 struct get_size_context
 {
 	binary_read_iterator* bri_p;
+	binary_read_iterator* cloned_bri_p;
 	const void* transaction_id;
 	int* abort_error;
 };
 
 static uint32_t read_from_stream_for_get_size_context(void* context_p, void* data, uint32_t data_size)
 {
-	get_size_context* c_p = context_p;
-	return read_from_binary_read_iterator(c_p->bri_p, data, data_size, c_p->transaction_id, c_p->abort_error);
+	if(data_size == 0) // this will never happen
+		return 0;
+
+	get_size_context* gsc_p = context_p;
+
+	// initialize the new iterator to not have need to forward the current one
+	if(gsc_p->cloned_bri_p == NULL)
+	{
+		gsc_p->cloned_bri_p = clone_binary_read_iterator(gsc_p->bri_p, gsc_p->transaction_id, gsc_p->abort_error);
+		if(*(gsc_p->abort_error))
+			return 0;
+	}
+
+	// read from the cloned_bri_p, this is will read no more than 4 bytes
+	// if abort_error, this function will return 0
+	return read_from_binary_read_iterator(gsc_p->cloned_bri_p, data, data_size, gsc_p->transaction_id, gsc_p->abort_error);
 }
 
 // the passed buffer must be atleast 4 bytes big, buffer_size will be set to bytes read from the bri_p
-static uint32_t get_next_tuple_size_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, char* buffer, uint32_t* buffer_size, const void* transaction_id, int* abort_error)
+uint32_t get_curr_tuple_size_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error)
 {
-	(*buffer_size) = 0;
-	return get_tuple_size_from_stream(tpl_d, buffer, buffer_size, &(get_size_context){bri_p, transaction_id, abort_error}, read_from_stream_for_get_size_context);
+	char buffer[4] = {};
+	uint32_t buffer_size = 0;
+	get_size_context gsc = {.bri_p = bri_p, .cloned_bri_p = NULL, .transaction_id = transaction_id, abort_error};
+
+	uint32_t curr_tuple_size = get_tuple_size_from_stream(tpl_d, buffer, &buffer_size, &gsc, read_from_stream_for_get_size_context);
+
+	// cloned_bri_p has to be deleted in any case, even if the get_tuple_size_from_stream() call failed with an abort_error
+	if(gsc.cloned_bri_p != NULL)
+		delete_binary_read_iterator(gsc.cloned_bri_p, transaction_id, abort_error);
+
+	if(*abort_error)
+		return 0;
+
+	return curr_tuple_size;
 }
 
 int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error)
 {
-	char buffer[4];
-	uint32_t bytes_read = 0;
-	const uint32_t bytes_to_read = get_next_tuple_size_from_binary_read_iterator(bri_p, tpl_d, buffer, &bytes_read, transaction_id, abort_error);
+	// get tuple_size of the tuple we are pointing to right now
+	const uint32_t tuple_size = get_curr_tuple_size_from_binary_read_iterator(bri_p, tpl_d, transaction_id, abort_error);
 	if(*abort_error)
 		return 0;
-
-	// end of stream OR error prone data
-	if(bytes_to_read == 0)
+	if(tuple_size == 0)
 		return 0;
 
-	// iterate will all of bytes_to_read are not skipped
-	while(bytes_read < bytes_to_read)
-	{
-		uint32_t bytes_read_this_iteration = read_from_binary_read_iterator(bri_p, NULL, bytes_to_read - bytes_read, transaction_id, abort_error);
-		if(*abort_error)
-			return 0;
-
-		if(bytes_read_this_iteration == 0)
-			return 0;
-
-		bytes_read += bytes_read_this_iteration;
-	}
+	// skip tuple_size number of bytes
+	uint32_t bytes_skipped = read_from_binary_read_iterator(bri_p, NULL, tuple_size, transaction_id, abort_error);
+	if(*abort_error)
+		return 0;
+	if(bytes_skipped < tuple_size)
+		return 0;
 
 	return 1;
 }
 
-void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error)
+const void* peek_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error)
 {
-	char buffer[4];
-	uint32_t bytes_read = 0;
-	const uint32_t bytes_to_read = get_next_tuple_size_from_binary_read_iterator(bri_p, tpl_d, buffer, &bytes_read, transaction_id, abort_error);
+	// get tuple_size of the tuple we are pointing to right now
+	const uint32_t tuple_size = get_curr_tuple_size_from_binary_read_iterator(bri_p, tpl_d, transaction_id, abort_error);
 	if(*abort_error)
 		return NULL;
-
-	// end of stream OR error prone data
-	if(bytes_to_read == 0)
+	if(tuple_size == 0)
 		return NULL;
 
-	void* tuple = malloc(bytes_to_read);
-	memory_move(tuple, buffer, bytes_read);
+	// peek tuple_size number of bytes, if you fall short, fail this call
+	uint32_t bytes_peeked = 0;
+	const void* tuple = peek_in_binary_read_iterator(bri_p, &bytes_peeked, transaction_id, abort_error);
+	if(*abort_error)
+		return NULL;
+	if(bytes_peeked < tuple_size) // if you are able to peek lesser bytes, fail this call
+		return NULL;
 
-	// iterate will all of bytes_to_read are not skipped
-	while(bytes_read < bytes_to_read)
+	return tuple;
+}
+
+void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error)
+{
+	// get tuple_size of the tuple we are pointing to right now
+	const uint32_t tuple_size = get_curr_tuple_size_from_binary_read_iterator(bri_p, tpl_d, transaction_id, abort_error);
+	if(*abort_error)
+		return NULL;
+	if(tuple_size == 0)
+		return NULL;
+
+	// malloc for tuple
+	void* tuple = malloc(tuple_size);
+	if(tuple == NULL)
+		exit(-1);
+
+	// read tuple_size number of bytes into it
+	uint32_t bytes_read = read_from_binary_read_iterator(bri_p, tuple, tuple_size, transaction_id, abort_error);
+	if(*abort_error)
 	{
-		uint32_t bytes_read_this_iteration = read_from_binary_read_iterator(bri_p, tuple + bytes_read, bytes_to_read - bytes_read, transaction_id, abort_error);
-		if(*abort_error)
-		{
-			free(tuple);
-			return NULL;
-		}
-
-		if(bytes_read_this_iteration == 0)
-		{
-			free(tuple);
-			return NULL;
-		}
-
-		bytes_read += bytes_read_this_iteration;
+		free(tuple);
+		return NULL;
+	}
+	if(bytes_read < tuple_size)
+	{
+		free(tuple);
+		return NULL;
 	}
 
 	return tuple;
