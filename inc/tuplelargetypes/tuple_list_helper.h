19fd0cd2fb625d8b72ba5e5a2c3ae1533b6c63ad
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-23 19:45:51 +0530

    new api for tuple_list finalized

--- inc/tuplelargetypes/tuple_list_helper.h
+++ inc/tuplelargetypes/tuple_list_helper.h
@@ -6,7 +6,7 @@
 // returns 1 if a tuple was skipped, else returns 0 for abort_error OR end of data
 int skip_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
 
-// returns a malloc-ed point to a next tuple read from the iterator, else returns NULL for abort_error OR end of data
-void* read_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, const void* transaction_id, int* abort_error);
+// returns a pointer to a next tuple read (free_the_pointer = 1) OR peeked (free_the_pointer = 0) from the iterator, else returns NULL for abort_error OR end of data
+void* get_tuple_from_binary_read_iterator(binary_read_iterator* bri_p, const tuple_def* tpl_d, int* free_the_pointer, const void* transaction_id, int* abort_error);
 
 #endif
