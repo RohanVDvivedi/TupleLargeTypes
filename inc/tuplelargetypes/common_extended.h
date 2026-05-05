a43c9a648b8198af8cca9d1e3ff0f231ab76cbf6
RohanVDvivedi <rohandvivedi@gmail.com>
2026-05-05 22:40:37 +0530

    common_extended header and source fixed

--- inc/tuplelargetypes/common_extended.h
+++ inc/tuplelargetypes/common_extended.h
@@ -5,13 +5,24 @@
 #include<tuplestore/data_type_info.h>
 #include<tupleindexer/common/page_access_specification.h>
 
-#define EXTENDED_TYPE_SUFFIX        "_extended"
-#define INLINE_TYPE_SUFFIX          "_inline"
+#define EXTENDED_TYPE_SUFFIX                 "_extended"
+#define INLINE_TYPE_SUFFIX                   "_inline"
 
-#define EXTENDED_PREFIX_POS_VAL          (0)
-#define EXTENDED_PREFIX_POS_ACC          STATIC_POSITION(EXTENDED_PREFIX_POS_VAL)
+#define EXTENDED_PREFIX_POS_VAL               (0)
+#define EXTENDED_PREFIX_POS_ACC               STATIC_POSITION(EXTENDED_PREFIX_POS_VAL)
 
-#define EXTENDED_HEAD_PAGE_ID_POS_ACC    STATIC_POSITION(1)
+#define EXTENSION_HEAD_POS_ACC                STATIC_POSITION(1)
+
+#define EXTENSION_HEAD_PAGE_ID_POS_ACC        STATIC_POSITION(1, 0)
+#define EXTENSION_HEAD_TUPLE_INDEX_POS_ACC    STATIC_POSITION(1, 1)
+
+typedef struct chunk_ptr chunk_ptr;
+struct chunk_ptr
+{
+	uint64_t page_id;
+	uint32_t tuple_index;
+	uint32_t byte_index;
+};
 
 // anything that is not extended is inline, including float, integer, etc
 int is_inline_type_info(const data_type_info* dti_p);
@@ -21,14 +32,10 @@ int is_extended_type_info(const data_type_info* dti_p);
 int has_extended_type_info(const data_type_info* dti_p);
 
 // returns 1-th element from the large type info
-uint64_t get_extension_head_page_id_for_extended_type(const datum* uval, const data_type_info* dti, const page_access_specs* pas_p);
+chunk_ptr get_extension_head_for_extended_type(const datum* uval, const data_type_info* dti, const page_access_specs* pas_p);
 
-#include<tupleindexer/worm/worm.h>
+#include<tupleindexer/blob_store/blob_store.h>
 
-// creates a new worm, and inserts it's head_page_id in the tupe at pos position and will also return it
-// does not create a new one if one already exists in the tuple for the pos position
-uint64_t get_or_create_extension_worm(void* tupl, const tuple_def* tpl_d, positional_accessor pos, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
-
-void delete_all_extension_worms(const datum* uval, const data_type_info* dti, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+void initialize_extension_head(void* tupl, const tuple_def* tpl_d, positional_accessor pos, const page_access_specs* pas_p);
 
 #endif
--- src/common_extended.c
+++ src/common_extended.c
@@ -34,7 +34,7 @@ int has_extended_type_info(const data_type_info* dti_p)
 
 #include<tuplelargetypes/numeric_extended.h>
 
-uint64_t get_extension_head_page_id_for_extended_type(const datum* uval, const data_type_info* dti, const page_access_specs* pas_p)
+chunk_ptr get_extension_head_for_extended_type(const datum* uval, const data_type_info* dti, const page_access_specs* pas_p)
 {
 	if(is_extended_type_info(dti) && !is_datum_NULL(uval)) // extract only when uval is not NULL && the type is the extended one
 	{
@@ -42,7 +42,7 @@ uint64_t get_extension_head_page_id_for_extended_type(const datum* uval, const d
 		const data_type_info* prefix_dti;
 		int valid = get_nested_containee_from_datum(&prefix, &prefix_dti, uval, dti, EXTENDED_PREFIX_POS_ACC);
 		if(!valid || is_datum_NULL(&prefix))
-			return pas_p->NULL_PAGE_ID;
+			return (chunk_ptr){pas_p->NULL_PAGE_ID};
 
 		if(is_numeric_extended_type_info(dti))
 		{
@@ -50,105 +50,40 @@ uint64_t get_extension_head_page_id_for_extended_type(const datum* uval, const d
 			const data_type_info* prefix_digits_dti;
 			int valid = get_nested_containee_from_datum(&prefix_digits, &prefix_digits_dti, uval, dti, GET_NUMERIC_DIGITS_POS_ACC(is_extended_type_info(dti)));
 			if(!valid || is_datum_NULL(&prefix_digits))
-				return pas_p->NULL_PAGE_ID;
+				return (chunk_ptr){pas_p->NULL_PAGE_ID};
 		}
 
-		datum extension_head_page_id;
-		const data_type_info* extension_head_page_id_dti;
-		valid = get_nested_containee_from_datum(&extension_head_page_id, &extension_head_page_id_dti, uval, dti, EXTENDED_HEAD_PAGE_ID_POS_ACC);
-		if(valid && (!is_datum_NULL(&extension_head_page_id)))
-			return extension_head_page_id.uint_value;
+		datum uval_head_page_id;
+		datum uval_head_tuple_index;
+		const data_type_info* dti;
+		if(get_nested_containee_from_datum(&uval_head_page_id, &dti, uval, dti, EXTENSION_HEAD_PAGE_ID_POS_ACC)
+			&& get_nested_containee_from_datum(&uval_head_tuple_index, &dti, uval, dti, EXTENSION_HEAD_TUPLE_INDEX_POS_ACC))
+			return (chunk_ptr){uval_head_page_id.uint_value, uval_head_tuple_index.uint_value};
 	}
 
-	return pas_p->NULL_PAGE_ID;
+	return (chunk_ptr){pas_p->NULL_PAGE_ID};
 }
 
-uint64_t get_or_create_extension_worm(void* tupl, const tuple_def* tpl_d, positional_accessor pos, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+void initialize_extension_head(void* tupl, const tuple_def* tpl_d, positional_accessor pos, const page_access_specs* pas_p)
 {
 	// make sure the type in context is an extended type
 	{
 		const data_type_info* dti_p = get_type_info_for_element_from_tuple_def(tpl_d, pos);
 		if(dti_p == NULL)
-			return pam_p->pas.NULL_PAGE_ID;
+			return ;
 		if(!is_extended_type_info(dti_p))
-			return pam_p->pas.NULL_PAGE_ID;
+			return ;
 	}
-
-	// initialize it to NULL
-	uint64_t extension_head_page_id = pam_p->pas.NULL_PAGE_ID;
 
 	// create a relative position accessor
 	relative_positional_accessor child_relative_accessor;
-	initialize_relative_positional_accessor(&child_relative_accessor, &pos, 1);
-	relative_positonal_accessor_set_from_relative(&child_relative_accessor, EXTENDED_HEAD_PAGE_ID_POS_ACC);
+	initialize_relative_positional_accessor(&child_relative_accessor, &pos, 2);
 
-	// fetch the extension_head_page_id
-	{
-		datum extension_head_page_id_uval;
-		if(!get_value_from_element_from_tuple(&extension_head_page_id_uval, tpl_d, child_relative_accessor.exact, tupl)) // if we can not extraxt one, fail this call
-		{
-			deinitialize_relative_positional_accessor(&child_relative_accessor);
-			return pam_p->pas.NULL_PAGE_ID;
-		}
-		if(is_datum_NULL(&extension_head_page_id_uval))
-			extension_head_page_id = pam_p->pas.NULL_PAGE_ID;
-		else
-			extension_head_page_id = extension_head_page_id_uval.uint_value;
-	}
+	relative_positonal_accessor_set_from_relative(&child_relative_accessor, EXTENSION_HEAD_PAGE_ID_POS_ACC);
+	set_element_in_tuple(tpl_d, child_relative_accessor.exact, tupl, &((datum){.uint_value = pas_p->NULL_PAGE_ID}), UINT32_MAX);
 
-	// if extension_head_page_id is NULL_PAGE_ID, then create a new worm and set it in the attribute beside prefix
-	if(extension_head_page_id == pam_p->pas.NULL_PAGE_ID)
-	{
-		extension_head_page_id = get_new_worm(1, pam_p->pas.NULL_PAGE_ID, wtd_p, pam_p, pmm_p, transaction_id, abort_error);
-		if(*abort_error)
-		{
-			deinitialize_relative_positional_accessor(&child_relative_accessor);
-			return pam_p->pas.NULL_PAGE_ID;
-		}
-		set_element_in_tuple(tpl_d, child_relative_accessor.exact, tupl, &((datum){.uint_value = extension_head_page_id}), UINT32_MAX);
-	}
+	relative_positonal_accessor_set_from_relative(&child_relative_accessor, EXTENSION_HEAD_TUPLE_INDEX_POS_ACC);
+	set_element_in_tuple(tpl_d, child_relative_accessor.exact, tupl, &((datum){.uint_value = 0}), UINT32_MAX);
 
 	deinitialize_relative_positional_accessor(&child_relative_accessor);
-	return extension_head_page_id;
-}
-
-void delete_all_extension_worms(const datum* uval, const data_type_info* dti, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
-{
-	// return directly if the uval is NULL
-	if(is_datum_NULL(uval))
-		return;
-
-	// if not a container, return directly
-	// skip a BINARY/STRING also here
-	if(dti->type != TUPLE && dti->type != ARRAY)
-		return;
-
-	// if already an extended type, delete an extension if it has any
-	if(is_extended_type_info(dti))
-	{
-		uint64_t extension_head_page_id = get_extension_head_page_id_for_extended_type(uval, dti, &(pam_p->pas));
-		if(extension_head_page_id != pam_p->pas.NULL_PAGE_ID)
-		{
-			uint64_t dependent_root_page_id;
-			int vaccum_needed;
-			decrement_reference_counter_for_worm(extension_head_page_id, &dependent_root_page_id, &vaccum_needed, wtd_p, pam_p, pmm_p, transaction_id, abort_error);
-			if(*abort_error)
-				return;
-		}
-		return;
-	}
-
-	uint32_t element_count = get_element_count_for_datum(uval, dti);
-	for(uint32_t i = 0; i < element_count; i++)
-	{
-		datum uval_c;
-		const data_type_info* dti_c;
-		int valid = get_containee_from_datum(&uval_c, &dti_c, uval, dti, i);
-		if(valid)
-		{
-			delete_all_extension_worms(&uval_c, dti_c, wtd_p, pam_p, pmm_p, transaction_id, abort_error);
-			if(*abort_error)
-				return;
-		}
-	}
 }
