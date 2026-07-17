e8b1ad3f788356955ac22790681f027ed410156e
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-17 11:21:31 +0530

    now tuple large types have 2 contexts

--- inc/tuplelargetypes/extension_reader_iterator_callback.h
+++ inc/tuplelargetypes/extension_reader_iterator_callback.h
@@ -4,7 +4,8 @@
 typedef struct extension_reader_iterator_callback extension_reader_iterator_callback;
 struct extension_reader_iterator_callback
 {
-	void* context;
+	void* context1;
+	void* context2;
 	void (*extension_blob_read_begin_event)(extension_reader_iterator_callback* callback, const datum* uval, const data_type_info* dti, const page_access_methods* pam_p);
 	void (*extension_blob_read_ended_event)(extension_reader_iterator_callback* callback, const datum* uval, const data_type_info* dti, const page_access_methods* pam_p);
 };
